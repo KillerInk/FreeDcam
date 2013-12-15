c7907a4c5bd160aabef196d94d551ee4c60d1426
troop <troop@email.de>
2013-12-15 20:04:47 +0100

    added native hdr rendering for 2d cam

--- src/com/troop/freecam/HDR/TwoDBitmapHandler.java
+++ src/com/troop/freecam/HDR/TwoDBitmapHandler.java
@@ -5,46 +5,38 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.widget.Toast;
-
+import com.jni.bitmap_operations.JniBitmapHolder;
 import com.troop.freecam.SavePictureTask;
-import com.troop.freecam.cm.HdrSoftwareProcessor;
-
+import com.troop.freecam.utils.BitmapUtils;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+
 
 /**
  * Created by troop on 19.11.13.
  */
 public class TwoDBitmapHandler extends BaseBitmapHandler
 {
+    JniBitmapHolder baseJni;
+    JniBitmapHolder highJni;
+    JniBitmapHolder lowJni;
+
     public TwoDBitmapHandler(Activity activity, Uri[] uris) {
         super(activity, uris);
     }
 
     public String render2d(String end, File sdcardpath)
     {
-        HdrSoftwareProcessor HdrRender = null;
-        try {
-            HdrRender = new HdrSoftwareProcessor(activity);
-            HdrRender.prepare(activity, uris);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        byte[] hdrpic = HdrRender.computeHDR(activity);
+        System.gc();
+        Runtime.getRuntime().gc();
+        System.gc();
+
+        baseJni.ToneMapImages(highJni, lowJni);
+        highJni.freeBitmap();
+        lowJni.freeBitmap();
+        Bitmap bitmap = baseJni.getBitmapAndFree();
+
         File file = SavePictureTask.getFilePath(end, sdcardpath);
-        FileOutputStream outStream = null;
-        try {
-            outStream = new FileOutputStream(file);
-            outStream.write(hdrpic);
-            outStream.flush();
-            outStream.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        BitmapUtils.saveBitmapToFile(file, bitmap);
         return file.getAbsolutePath();
     }
 
@@ -56,12 +48,12 @@ public class TwoDBitmapHandler extends BaseBitmapHandler
 
         try
         {
-            Bitmap newFirstPic = Bitmap.createBitmap(BitmapFactory.decodeFile(uris[0].getPath()), first.X, first.Y, first.Width, first.Height);
-            saveBitmap(uris[0].getPath(), newFirstPic);
-            Bitmap newSecondPic = Bitmap.createBitmap(BitmapFactory.decodeFile(uris[2].getPath()), second.X, second.Y, second.Width, second.Height);
-            saveBitmap(uris[2].getPath(), newSecondPic);
-            Bitmap newBaseImage = Bitmap.createBitmap(BitmapFactory.decodeFile(uris[1].getPath()), base.X, base.Y, base.Width, base.Height);
-            saveBitmap(uris[1].getPath(), newBaseImage);
+            baseJni = new JniBitmapHolder(BitmapFactory.decodeFile(uris[1].getPath()));
+            highJni = new JniBitmapHolder(BitmapFactory.decodeFile(uris[0].getPath()));
+            lowJni  = new JniBitmapHolder(BitmapFactory.decodeFile(uris[2].getPath()));
+            baseJni.cropBitmap(base.X, base.Y, base.Width, base.Height);
+            highJni.cropBitmap(first.X, first.Y, first.Width, first.Height);
+            lowJni.cropBitmap(second.X, second.Y, second.Width, second.Height);
         }
         catch (OutOfMemoryError ex)
         {
