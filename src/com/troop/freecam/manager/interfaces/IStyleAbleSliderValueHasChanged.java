59ccf66a10a365a65983d0fcd87b66f6861b9027
troop <troop@email.de>
2013-12-28 09:12:54 +0100

    added styleableslyder to source

---
+++ src/com/troop/freecam/manager/interfaces/IStyleAbleSliderValueHasChanged.java
@@ -0,0 +1,9 @@
+package com.troop.freecam.manager.interfaces;
+
+/**
+ * Created by troop on 23.12.13.
+ */
+public interface IStyleAbleSliderValueHasChanged
+{
+    public void ValueHasChanged(int value);
+}
