73438377f80483b49feae3667272890e4117a68c
troop <trooopiii@googlemail.com>
2014-08-16 10:53:06 +0200

    added open/close camera logic to CamerUIWrapper

--- src/com/troop/freecam/camera/CameraUiWrapper.java
+++ src/com/troop/freecam/camera/CameraUiWrapper.java
@@ -1,11 +1,13 @@
 package com.troop.freecam.camera;
 
+import android.hardware.Camera;
 import android.view.SurfaceHolder;
 
 import com.troop.freecam.camera.modules.ModuleHandler;
 import com.troop.freecam.manager.AppSettingsManager;
 import com.troop.freecam.manager.SoundPlayer;
 import com.troop.freecam.surfaces.CamPreview;
+import com.troop.freecam.utils.DeviceUtils;
 
 /**
  * Created by troop on 16.08.2014.
@@ -32,7 +34,12 @@ public class CameraUiWrapper implements SurfaceHolder.Callback
 
 //SURFACEHOLDER Interface START
     @Override
-    public void surfaceCreated(SurfaceHolder holder) {
+    public void surfaceCreated(SurfaceHolder holder)
+    {
+        if (openCamera())
+        {
+
+        }
 
     }
 
@@ -43,8 +50,53 @@ public class CameraUiWrapper implements SurfaceHolder.Callback
 
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
-
+        cameraHolder.CloseCamera();
     }
 
 //SURFACEHOLDER Interface END
+
+
+//Module Handler START
+    public void SwitchModule(String moduleName)
+    {
+        moduleHandler.SetModule(moduleName);
+    }
+
+    public void DoWork()
+    {
+        moduleHandler.DoWork();
+    }
+
+//Module Handler END
+
+
+    private boolean openCamera()
+    {
+        if (Camera.getNumberOfCameras() == 3)
+        {
+            if (appSettingsManager.Cameras.GetCamera().equals(AppSettingsManager.Preferences.MODE_3D)) {
+                if (DeviceUtils.isEvo3d())
+                    return cameraHolder.OpenCamera(100);
+                else
+                    return cameraHolder.OpenCamera(2);
+            } else if (appSettingsManager.Cameras.GetCamera().equals(AppSettingsManager.Preferences.MODE_2D))
+                return cameraHolder.OpenCamera(0);
+            else if (appSettingsManager.Cameras.GetCamera().equals(AppSettingsManager.Preferences.MODE_Front))
+                return cameraHolder.OpenCamera(1);
+        }
+        else if(Camera.getNumberOfCameras() == 2)
+        {
+            if (appSettingsManager.Cameras.GetCamera().equals(AppSettingsManager.Preferences.MODE_2D))
+                return cameraHolder.OpenCamera(0);
+            else if (appSettingsManager.Cameras.GetCamera().equals(AppSettingsManager.Preferences.MODE_Front))
+                return cameraHolder.OpenCamera(1);
+        }
+        else if(Camera.getNumberOfCameras() == 1)
+        {
+            return cameraHolder.OpenCamera(1);
+        }
+
+        return false;
+    }
+
 }
