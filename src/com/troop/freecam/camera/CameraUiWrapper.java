df0c86b2ea4269baf88c30a038d6daedb53dbe3b
troop <trooopiii@googlemail.com>
2014-08-16 09:23:15 +0200

    added surface callback to cameruiwrapper

--- src/com/troop/freecam/camera/CameraUiWrapper.java
+++ src/com/troop/freecam/camera/CameraUiWrapper.java
@@ -1,5 +1,7 @@
 package com.troop.freecam.camera;
 
+import android.view.SurfaceHolder;
+
 import com.troop.freecam.camera.modules.ModuleHandler;
 import com.troop.freecam.manager.AppSettingsManager;
 import com.troop.freecam.manager.SoundPlayer;
@@ -8,7 +10,7 @@ import com.troop.freecam.surfaces.CamPreview;
 /**
  * Created by troop on 16.08.2014.
  */
-public class CameraUiWrapper
+public class CameraUiWrapper implements SurfaceHolder.Callback
 {
     private CamPreview preview;
     private ModuleHandler moduleHandler;
@@ -22,7 +24,27 @@ public class CameraUiWrapper
         this.preview = preview;
         this.soundPlayer = soundPlayer;
         this.appSettingsManager = appSettingsManager;
+        //attache the callback to the Campreview
+        preview.getHolder().addCallback(this);
         cameraHolder = new BaseCameraHolder();
         moduleHandler = new ModuleHandler(cameraHolder, appSettingsManager, soundPlayer);
     }
+
+//SURFACEHOLDER Interface START
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
+
+//SURFACEHOLDER Interface END
 }
