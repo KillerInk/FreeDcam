11469a3fd70f8d2e98b3f6e65869aacc14a6dbe6
troop <trooopiii@googlemail.com>
2014-08-16 09:00:14 +0200

    added newcameramanger, this replace the cameramanager

---
+++ src/com/troop/freecam/camera/CameraManagerNew.java
@@ -0,0 +1,28 @@
+package com.troop.freecam.camera;
+
+import com.troop.freecam.camera.modules.ModuleHandler;
+import com.troop.freecam.manager.AppSettingsManager;
+import com.troop.freecam.manager.SoundPlayer;
+import com.troop.freecam.surfaces.CamPreview;
+
+/**
+ * Created by troop on 16.08.2014.
+ */
+public class CameraManagerNew
+{
+    private CamPreview preview;
+    private ModuleHandler moduleHandler;
+    private BaseCameraHolder cameraHolder;
+    AppSettingsManager appSettingsManager;
+    SoundPlayer soundPlayer;
+
+
+    public CameraManagerNew(CamPreview preview, AppSettingsManager appSettingsManager, SoundPlayer soundPlayer)
+    {
+        this.preview = preview;
+        this.soundPlayer = soundPlayer;
+        this.appSettingsManager = appSettingsManager;
+        cameraHolder = new BaseCameraHolder();
+        moduleHandler = new ModuleHandler(cameraHolder, appSettingsManager, soundPlayer);
+    }
+}
