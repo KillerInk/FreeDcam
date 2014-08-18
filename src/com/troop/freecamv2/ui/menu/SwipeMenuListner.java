933cd770050936bef95b75aaea793997f0653907
troop <trooopiii@googlemail.com>
2014-08-18 13:05:16 +0200

    added animation to menu open close

--- src/com/troop/freecamv2/ui/menu/SwipeMenuListner.java
+++ src/com/troop/freecamv2/ui/menu/SwipeMenuListner.java
@@ -1,5 +1,7 @@
 package com.troop.freecamv2.ui.menu;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.LinearLayout;
@@ -12,54 +14,61 @@ public class SwipeMenuListner
     LinearLayout settingsLayout;
     LinearLayout manualSettingsLayout;
 
-    final int distance = 100;
+    final int distance = 200;
     int startX;
     int startY;
     int currentX;
     int currentY;
+    boolean verticalSwipe = false;
+    boolean horizontalSwipe = false;
+    int mShortAnimationDuration;
+    boolean animationRunning = false;
 
     public SwipeMenuListner(LinearLayout settingsLayout, LinearLayout manualSettingsLayout)
     {
         this.manualSettingsLayout = manualSettingsLayout;
         this.settingsLayout = settingsLayout;
+        mShortAnimationDuration = settingsLayout.getResources().getInteger(
+                android.R.integer.config_mediumAnimTime);
+
     }
 
 
     public boolean onTouchEvent(MotionEvent event)
     {
         boolean fireagain = true;
-        switch (event.getAction())
-        {
-            case MotionEvent.ACTION_DOWN:
-                startX = (int)event.getX();
-                startY = (int)event.getY();
-                currentX = (int)event.getX();
-                currentY = (int)event.getY();
-                break;
-            case MotionEvent.ACTION_MOVE:
-                currentX = (int) event.getX();
-                currentY = (int) event.getY();
-                dectectSwipe();
-                break;
-            case MotionEvent.ACTION_UP:
-                fireagain = false;
-                break;
+        if (!animationRunning) {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    startX = (int) event.getX();
+                    startY = (int) event.getY();
+                    currentX = (int) event.getX();
+                    currentY = (int) event.getY();
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    currentX = (int) event.getX();
+                    currentY = (int) event.getY();
+                    detectSwipeDirection();
+                    break;
+                case MotionEvent.ACTION_UP:
+                    fireagain = false;
+                    break;
+            }
         }
 
         return fireagain;
     }
 
-    private void dectectSwipe()
+    private void detectSwipeDirection()
     {
-        if (startX < currentX && getDistance(startX, currentX) > 100)
-            doLeftToRightSwipe();
-        else if (startX > currentX && getDistance(startX, currentX) > 100)
-            doRightToLeftSwipe();
-        else if (startY > currentY && getDistance(startY, currentY) > 100)
-            doBottomToTopSwipe();
-        else if (startY < currentY && getDistance(startY, currentY) > 100)
-            doTopToBottomSwipe();
-
+        int x = getDistance(startX, currentX);
+        int y = getDistance(startY, currentY);
+        if (x >= distance || y >= distance) {
+            if (x >= y)
+                doHorizontalSwipe();
+            else
+                doVerticalSwipe();
+        }
     }
 
 
@@ -71,20 +80,75 @@ public class SwipeMenuListner
         return dis;
     }
 
-    private void doLeftToRightSwipe()
+    private void doHorizontalSwipe()
     {
-        settingsLayout.setVisibility(View.VISIBLE);
+        if (startX - currentX > 0)
+        {
+            animationRunning = true;
+            settingsLayout.setAlpha(1f);
+            settingsLayout.animate()
+                    .alpha(0f)
+                    .setDuration(mShortAnimationDuration)
+                    .setListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            settingsLayout.setVisibility(View.GONE);
+                            settingsLayout.animate().setListener(null);
+                            animationRunning =false;
+                        }
+                    });
+        }
+        else
+        {
+            settingsLayout.setAlpha(0f);
+            settingsLayout.setVisibility(View.VISIBLE);
+            settingsLayout.animate()
+                    .alpha(1f)
+                    .setDuration(mShortAnimationDuration)
+                    .setListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation)
+                        {
+                            settingsLayout.animate().setListener(null);
+                            animationRunning = false;
+                        }
+                    });
+        }
     }
-    private void doRightToLeftSwipe()
+
+    private void doVerticalSwipe()
     {
-        settingsLayout.setVisibility(View.GONE);
-    }
-    private void doTopToBottomSwipe()
-    {
-        manualSettingsLayout.setVisibility(View.VISIBLE);
-    }
-    private void doBottomToTopSwipe()
-    {
-        manualSettingsLayout.setVisibility(View.GONE);
+        if (startY - currentY > 0)
+        {
+            animationRunning = true;
+            manualSettingsLayout.setAlpha(1f);
+            manualSettingsLayout.animate()
+                    .alpha(0f)
+                    .setDuration(mShortAnimationDuration)
+                    .setListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            manualSettingsLayout.setVisibility(View.GONE);
+                            manualSettingsLayout.animate().setListener(null);
+                            animationRunning = false;
+                        }
+                    });
+        }
+        else
+        {
+            animationRunning = true;
+            manualSettingsLayout.setAlpha(0f);
+            manualSettingsLayout.setVisibility(View.VISIBLE);
+            manualSettingsLayout.animate()
+                    .alpha(1f)
+                    .setDuration(mShortAnimationDuration)
+                    .setListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            manualSettingsLayout.animate().setListener(null);
+                            animationRunning = false;
+                        }
+                    });
+        }
     }
 }
