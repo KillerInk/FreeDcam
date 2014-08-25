7c34b2a1abfa51d8ee96b597533386311acd9641
troop <trooopiii@googlemail.com>
2014-08-25 18:55:51 +0200

    adding thumbview

--- src/com/troop/freecamv2/ui/MainActivity_v2.java
+++ src/com/troop/freecamv2/ui/MainActivity_v2.java
@@ -1,5 +1,10 @@
 package com.troop.freecamv2.ui;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
@@ -7,12 +12,16 @@ import android.widget.ImageView;
 
 import com.troop.freecam.R;
 
+import com.troop.freecam.manager.MediaScannerManager;
 import com.troop.freecamv2.camera.CameraUiWrapper;
+import com.troop.freecamv2.camera.modules.I_WorkEvent;
 import com.troop.freecamv2.ui.TextureView.ExtendedSurfaceView;
 import com.troop.freecamv2.ui.menu.MenuHandler;
 import com.troop.freecamv2.ui.switches.CameraSwitchHandler;
 import com.troop.freecamv2.ui.switches.FlashSwitchHandler;
 import com.troop.freecamv2.ui.switches.ModuleSwitchHandler;
+
+import java.io.File;
 
 /**
  * Created by troop on 18.08.2014.
@@ -27,6 +36,9 @@ public class MainActivity_v2 extends MenuVisibilityActivity
     CameraSwitchHandler cameraSwitchHandler;
     ModuleSwitchHandler moduleSwitchHandler;
     FlashSwitchHandler flashSwitchHandler;
+    Activity activity;
+    ImageView thumbView;
+    File lastFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +47,7 @@ public class MainActivity_v2 extends MenuVisibilityActivity
         appSettingsManager = new AppSettingsManager(PreferenceManager.getDefaultSharedPreferences(this));
         cameraPreview = (ExtendedSurfaceView)findViewById(R.id.CameraPreview);
         cameraUiWrapper = new CameraUiWrapper(cameraPreview, appSettingsManager,null);
+        cameraUiWrapper.moduleHandler.moduleEventHandler.AddWorkFinishedListner(cameraModuleHasWorkFinished);
         menuHandler = new MenuHandler(this, cameraUiWrapper, appSettingsManager);
 
         shutterButton = (ImageView)findViewById(R.id.shutter_imageview);
@@ -48,7 +61,9 @@ public class MainActivity_v2 extends MenuVisibilityActivity
         cameraSwitchHandler = new CameraSwitchHandler(this, cameraUiWrapper, appSettingsManager, cameraPreview);
         moduleSwitchHandler = new ModuleSwitchHandler(this, cameraUiWrapper, appSettingsManager);
         flashSwitchHandler = new FlashSwitchHandler(this, cameraUiWrapper, appSettingsManager);
-
+        activity = this;
+        thumbView = (ImageView)findViewById(R.id.imageView_Thumbnail);
+        thumbView.setOnClickListener(onThumbViewClick);
     }
 
     @Override
@@ -61,6 +76,65 @@ public class MainActivity_v2 extends MenuVisibilityActivity
     protected void onPause() {
         super.onPause();
 
+    }
+
+    private I_WorkEvent cameraModuleHasWorkFinished = new I_WorkEvent() {
+        @Override
+        public String WorkHasFinished(File filePath)
+        {
+            lastFile = filePath;
+            MediaScannerManager.ScanMedia(activity, filePath);
+            thumbView.setImageBitmap(loadThumbViewImage(filePath));
+            return null;
+        }
+    };
+
+    private View.OnClickListener onThumbViewClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v)
+        {
+            if (lastFile != null)
+            {
+                Uri uri = Uri.fromFile(lastFile);
+                Intent i=new Intent(Intent.ACTION_VIEW);
+                if (lastFile.getAbsolutePath().endsWith("mp4"))
+                    i.setDataAndType(uri, "video/*");
+                else
+                    i.setDataAndType(uri, "image/*");
+                startActivity(i);
+            }
+        }
+    };
+
+    private Bitmap loadThumbViewImage(File file)
+    {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        Bitmap bitmaporg = BitmapFactory.decodeFile(file.getAbsolutePath(), options);
+        options.inSampleSize = calculateInSampleSize(options, thumbView.getWidth(), thumbView.getHeight());
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeFile(file.getAbsolutePath(), options);
+    }
+
+    public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight)
+    {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            // Calculate ratios of height and width to requested height and width
+            final int heightRatio = Math.round((float) height / (float) reqHeight);
+            final int widthRatio = Math.round((float) width / (float) reqWidth);
+
+            // Choose the smallest ratio as inSampleSize value, this will guarantee
+            // a final image with both dimensions larger than or equal to the
+            // requested height and width.
+            inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+        }
+        return inSampleSize;
     }
 
 }
