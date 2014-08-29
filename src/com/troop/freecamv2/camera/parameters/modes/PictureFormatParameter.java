1b2f54ab0823a7542a6e46df9de3257b694ec475
troop <trooopiii@googlemail.com>
2014-08-29 16:52:27 +0200

    dont show bayer-qcom picture formats, they freez the picturecallback if used

--- src/com/troop/freecamv2/camera/parameters/modes/PictureFormatParameter.java
+++ src/com/troop/freecamv2/camera/parameters/modes/PictureFormatParameter.java
@@ -4,6 +4,8 @@ import android.hardware.Camera;
 
 import com.troop.freecamv2.camera.parameters.I_ParameterChanged;
 
+import java.util.ArrayList;
+
 /**
  * Created by troop on 24.08.2014.
  */
@@ -12,5 +14,18 @@ public class PictureFormatParameter extends BaseModeParameter
 
     public PictureFormatParameter(Camera.Parameters parameters, I_ParameterChanged parameterChanged, String value, String values) {
         super(parameters, parameterChanged, value, values);
+    }
+
+    @Override
+    public String[] GetValues()
+    {
+        ArrayList<String> toReturn = new ArrayList<String>();
+        final String[] vals = parameters.get(values).split(",");
+        for (int i = 0; i < vals.length; i++)
+        {
+            if (!vals[i].startsWith("bayer-qcom"))
+                toReturn.add(vals[i]);
+        }
+        return toReturn.toArray(new String[toReturn.size()]);
     }
 }
