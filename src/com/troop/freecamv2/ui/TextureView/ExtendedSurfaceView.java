30759c201122d7ccb345a256c9ee3babc4770a25
troop <trooopiii@googlemail.com>
2014-09-07 16:02:53 +0200

    fixing preview appearing for 4:3 ratio and setting mainbackground to black

--- src/com/troop/freecamv2/ui/TextureView/ExtendedSurfaceView.java
+++ src/com/troop/freecamv2/ui/TextureView/ExtendedSurfaceView.java
@@ -2,13 +2,17 @@ package com.troop.freecamv2.ui.TextureView;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Point;
+import android.os.Build;
 import android.preference.PreferenceManager;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Display;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.TextureView;
+import android.view.WindowManager;
 import android.widget.RelativeLayout;
 
 import com.lge.real3d.Real3D;
@@ -111,23 +115,58 @@ public class ExtendedSurfaceView extends SurfaceView implements I_PreviewSizeEve
     @Override
     public void OnPreviewSizeChanged(int w, int h)
     {
+
         //[1.00 = square] [1.25 = 5:4] [1.33 = 4:3] [1.50 = 3:2] [1.60 = 16:10] [1.67 = 5:3] [1.71 = 128:75] [1.78 = 16:9] [1.85] [2.33 = 21:9 (1792x768)] [2.35 = Cinamascope] [2.37 = "21:9" (2560x1080)] [2.39 = Panavision]
         double newratio = (double)w/(double)h;
-        DisplayMetrics metrics = getResources().getDisplayMetrics();
-        double displayratio = (double)metrics.widthPixels/(double)metrics.heightPixels;
+        newratio = Math.round(newratio*100.0)/100.0;
+        int width = 0;
+        int height = 0;
+
+        if (Build.VERSION.SDK_INT >= 17)
+        {
+            WindowManager wm = (WindowManager)context.getSystemService(Context.WINDOW_SERVICE);
+            Point size =  new Point();
+            wm.getDefaultDisplay().getRealSize(size);
+            width = size.x;
+            height = size.y;
+        }
+        else
+        {
+            DisplayMetrics metrics = getResources().getDisplayMetrics();
+            width = metrics.widthPixels;
+            height = metrics.heightPixels;
+
+        }
+        double displayratio = (double)width/(double)height;
+        displayratio = Math.round(displayratio*100.0)/100.0;
+
+
+
         if (newratio == displayratio)
         {
-            RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(metrics.widthPixels, metrics.heightPixels);
+            RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(width, height);
             layoutParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT, RelativeLayout.TRUE);
             layoutParams.addRule(RelativeLayout.ALIGN_PARENT_TOP, RelativeLayout.TRUE);
             layoutParams.rightMargin = 0;
+            layoutParams.leftMargin = 0;
+            this.setLayoutParams(layoutParams);
+        }
+        else if (newratio == 1.33)
+        {
+            int tmo = (int)((double)width / displayratio * newratio);
+            int newwidthdiff = width - tmo;
+            RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(width, height);
+            layoutParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT, RelativeLayout.TRUE);
+            layoutParams.addRule(RelativeLayout.ALIGN_PARENT_TOP, RelativeLayout.TRUE);
+            layoutParams.rightMargin = newwidthdiff/2;
+            layoutParams.leftMargin = newwidthdiff/2;
             this.setLayoutParams(layoutParams);
         }
         else
         {
-            int tmo = (int)((double)metrics.widthPixels / displayratio * newratio);
-            int newwidthdiff = metrics.widthPixels - tmo;
-            RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(metrics.widthPixels, metrics.heightPixels);
+            int tmo = (int)((double)width / displayratio * newratio);
+            int newwidthdiff = width - tmo;
+            RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(width, height);
             layoutParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT, RelativeLayout.TRUE);
             layoutParams.addRule(RelativeLayout.ALIGN_PARENT_TOP, RelativeLayout.TRUE);
             layoutParams.rightMargin = newwidthdiff;
