2dbc86b9b3faae857590e434e396ac4948793afd
troop <trooopiii@googlemail.com>
2014-10-21 12:36:30 +0200

    Revert "if no valid preview size where found set it to max value"
    This reverts commit 338634e22fa20fc48f9bd35855af519f9cbbafc4.

--- src/com/troop/freecamv2/ui/TextureView/ExtendedSurfaceView.java
+++ src/com/troop/freecamv2/ui/TextureView/ExtendedSurfaceView.java
@@ -145,16 +145,10 @@ public class ExtendedSurfaceView extends SurfaceView implements I_PreviewSizeEve
                 break;
             }
         }
-        if (foundmatchingPreview == false)
-        {
+        if (foundmatchingPreview == false) {
             Log.d(TAG, "Found no matching preview size, raw capture will fail");
             String msg = "Found no matching preview size, raw capture will fail";
             ParametersHandler.cameraHolder.errorHandler.OnError(msg);
-            String[] split = previewSizes[previewSizes.length].split("x");
-            int pw = Integer.parseInt(split[0]);
-            int ph = Integer.parseInt(split[1]);
-            ParametersHandler.PreviewSize.SetValue(previewSizes[previewSizes.length]);
-            setPreviewToDisplay(pw, ph);
         }
         //[1.00 = square] [1.25 = 5:4] [1.33 = 4:3] [1.50 = 3:2] [1.60 = 16:10] [1.67 = 5:3] [1.71 = 128:75] [1.78 = 16:9] [1.85] [2.33 = 21:9 (1792x768)] [2.35 = Cinamascope] [2.37 = "21:9" (2560x1080)] [2.39 = Panavision]
 
