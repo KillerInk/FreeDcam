fc30ed8cf27e69fa9b2b508375bfbd0abc358049
troop <trooopiii@googlemail.com>
2014-10-28 12:21:31 +0100

    maybe this fix m8 manualshutter

--- src/com/troop/freedcamv2/camera/parameters/manual/ShutterManualParameter.java
+++ src/com/troop/freedcamv2/camera/parameters/manual/ShutterManualParameter.java
@@ -11,8 +11,12 @@ import com.troop.freedcamv2.utils.DeviceUtils;
  */
 public class ShutterManualParameter extends BaseManualParameter
 {
-    public static String HTCShutterValues = "4.0,3.2,2.5,1.6,1.3,1.0,0.8,0.6,0.5,0.4,0.3,0.25,0.2,0.125,0.1,0.07,0.06,0.05,0.04,0.03,0.025,0.02,0.015,0.0125,0.01,1/125"+
-            ",1/200,1/250,1/300,1/400,1/500,1/640,1/800,1/1000,1/1250,1/1600,1/2000,1/2500,1/3200,1/4000,1/5000,1/6400,1/8000";
+    /*M8 Stuff
+    //M_SHUTTER_SPEED_MARKER=1/8000,1/1000,1/125,1/15,0.5,4 ???
+    //return cameraController.getStringCameraParameter("shutter-threshold");
+    */
+    String TAG = "freedcam.ShutterManualParameter";
+    public static String HTCShutterValues = "1/8000,1/6400,1/5000,1/4000,1/3200,1/2500,1/2000,1/1600,1/1250,1/1000,1/800,1/640,1/500,1/400,1/320,1/250,1/200,1/125,1/100,1/80,1/60,1/50,1/40,1/30,1/25,1/20,1/15,1/13,1/10,1/8,1/6,1/5,1/4,0.3,0.4,0.5,0.6,0.8,1,1.3,1.6,2,2.5,3.2,4";
     
     public static String Z5SShutterValues = "31.0,30.0,29.0,28.0,27.0,26.0,25.0,24.0,23.0,22.0,21.0,"+
     										"20.0,19.0,18.0,17.0,16.0,15.0,14.0,13.0,12.0,11.0,10.0" +
@@ -48,9 +52,10 @@ public class ShutterManualParameter extends BaseManualParameter
         super(parameters, value, maxValue, MinValue);
 
         this.baseCameraHolder = baseCameraHolder;
+        if (DeviceUtils.isHTCADV())
         {
             this.isSupported = true;
-            shutterValues = Z5SShutterValues.split(",");
+            shutterValues = HTCShutterValues.split(",");
         }
         if (DeviceUtils.isZTEADV())
         {
@@ -97,11 +102,6 @@ public class ShutterManualParameter extends BaseManualParameter
     		parameters.set("sony-shutter-speed", valueToSet);
     		
         }*/
-    	
-    	//Do Not Merge HTC Was Disabled
-        
-        
-        	Log.e("", "Shutter Entered");
             current = valueToSet;
             String shutterstring = shutterValues[current];
             if (shutterstring.contains("/"))
@@ -110,14 +110,11 @@ public class ShutterManualParameter extends BaseManualParameter
                 float a = Float.parseFloat(split[0])/ Float.parseFloat(split[1]);
                 shutterstring = ""+a;
             }
-            Log.e("AE", "ae-af");
-
-           // parameters.set("night_exposure_mode", 0);
         if (DeviceUtils.isZTEADV())
             parameters.set("slow_shutter", shutterstring);
         if (DeviceUtils.isHTCADV())
             parameters.set("shutter", shutterstring);
-        Log.e("exposure", shutterstring);
+        Log.e(TAG, shutterstring);
 
        
     }
@@ -144,6 +141,7 @@ public class ShutterManualParameter extends BaseManualParameter
         if (DeviceUtils.isHTCADV()||DeviceUtils.isZTEADV()) {
             parameters.set("zsl", "off");
             parameters.set("auto-exposure", "center-weighted");
+            parameters.set("shutter-threshold", "0.2");
         }
         if (DeviceUtils.isZTEADV())
             parameters.set("slow_shutter_addition", 0);
