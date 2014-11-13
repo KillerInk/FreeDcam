4d3d0b77c67f33e04d69977f946c6a0a24f535b5
troop <trooopiii@googlemail.com>
2014-11-13 12:25:01 +0100

    add longexposrue child to fix app start ist not null

---
+++ src/com/troop/freedcam/ui/menu/childs/LongExposureChild.java
@@ -0,0 +1,51 @@
+package com.troop.freedcam.ui.menu.childs;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+
+import com.troop.freedcam.camera.CameraUiWrapper;
+import com.troop.freedcam.camera.parameters.modes.I_ModeParameter;
+import com.troop.freedcam.ui.AppSettingsManager;
+
+import java.util.ArrayList;
+
+/**
+ * Created by troop on 13.11.2014.
+ */
+public class LongExposureChild extends ExpandableChild {
+    public LongExposureChild(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public LongExposureChild(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public LongExposureChild(Context context) {
+        super(context);
+    }
+
+    @Override
+    public void setParameterHolder(I_ModeParameter parameterHolder, AppSettingsManager appSettingsManager, String settingsname, ArrayList<String> modulesToShow, CameraUiWrapper cameraUiWrapper) {
+        this.appSettingsManager = appSettingsManager;
+        this.settingsname = settingsname;
+        this.cameraUiWrapper = cameraUiWrapper;
+        String settingValue = appSettingsManager.getString(settingsname);
+        if (settingValue.equals("")) {
+            appSettingsManager.setString(settingsname, "1");
+            Log.d(getTAG(), "No appSetting set default " + Name + ":" + 1);
+        }
+        nameTextView.setText(Name);
+        valueTextView.setText(appSettingsManager.getString(settingsname));
+        AddModulesToShow(modulesToShow);
+    }
+
+    @Override
+    public void setValue(String value) {
+        valueTextView.setText(value);
+        //parameterHolder.SetValue(value, true);
+        appSettingsManager.setString(settingsname, value);
+        Log.d(getTAG(), "Set " + Name + ":" + value);
+    }
+}
