68b65c4139d8c1ee962066c0c0e82c60756a569e
troop <trooopiii@googlemail.com>
2014-12-11 19:34:14 +0100

    first sony stuff for detections wifi device detection

---
+++ app/src/main/java/com/troop/freedcam/sonyapi/CameraUiWrapperSony.java
@@ -0,0 +1,9 @@
+package com.troop.freedcam.sonyapi;
+
+import com.troop.freedcam.i_camera.AbstractCameraUiWrapper;
+
+/**
+ * Created by troop on 11.12.2014.
+ */
+public class CameraUiWrapperSony  extends AbstractCameraUiWrapper{
+}
