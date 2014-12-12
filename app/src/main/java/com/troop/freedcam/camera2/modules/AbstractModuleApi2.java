29ae7daab87ab0a71442b81a0cedc2c0201fc0d9
troop <trooopiii@googlemail.com>
2014-12-12 13:44:11 +0100

    add abstractmodule for api2 and parametershandlerapi2

---
+++ app/src/main/java/com/troop/freedcam/camera2/modules/AbstractModuleApi2.java
@@ -0,0 +1,56 @@
+package com.troop.freedcam.camera2.modules;
+
+import com.troop.freedcam.camera.BaseCameraHolder;
+import com.troop.freedcam.camera.modules.ModuleEventHandler;
+import com.troop.freedcam.camera2.BaseCameraHolderApi2;
+import com.troop.freedcam.i_camera.modules.I_Module;
+import com.troop.freedcam.i_camera.parameters.AbstractParameterHandler;
+import com.troop.freedcam.ui.AppSettingsManager;
+
+/**
+ * Created by troop on 12.12.2014.
+ */
+public abstract class AbstractModuleApi2 implements I_Module
+{
+    protected BaseCameraHolderApi2 baseCameraHolder;
+    protected AppSettingsManager Settings;
+    protected AbstractParameterHandler ParameterHandler;
+
+    protected boolean isWorking = false;
+    protected String name;
+
+    protected ModuleEventHandler eventHandler;
+
+    public AbstractModuleApi2(BaseCameraHolderApi2 cameraHandler, AppSettingsManager Settings, ModuleEventHandler eventHandler)
+    {
+        this.baseCameraHolder = cameraHandler;
+        this.Settings = Settings;
+        this.eventHandler = eventHandler;
+        this.ParameterHandler = baseCameraHolder.ParameterHandler;
+    }
+
+    @Override
+    public String ModuleName() {
+        return name;
+    }
+
+    @Override
+    public void DoWork() {
+
+    }
+
+    @Override
+    public boolean IsWorking() {
+        return isWorking;
+    }
+
+    @Override
+    public void LoadNeededParameters() {
+
+    }
+
+    @Override
+    public void UnloadNeededParameters() {
+
+    }
+}
---
+++ app/src/main/java/com/troop/freedcam/camera2/parameters/ParameterHandlerApi2.java
@@ -0,0 +1,11 @@
+package com.troop.freedcam.camera2.parameters;
+
+import com.troop.freedcam.i_camera.parameters.AbstractParameterHandler;
+
+/**
+ * Created by troop on 12.12.2014.
+ */
+public class ParameterHandlerApi2 extends AbstractParameterHandler
+{
+
+}
