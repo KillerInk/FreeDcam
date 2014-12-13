ac3ace734b04e2828952d07a2db0807b3cca5804
troop <trooopiii@googlemail.com>
2014-12-13 20:35:50 +0100

    sony:first wrapping around the api and try to get preview working

--- app/src/main/java/com/troop/freedcam/sonyapi/CameraHolderSony.java
+++ app/src/main/java/com/troop/freedcam/sonyapi/CameraHolderSony.java
@@ -1,17 +1,114 @@
 package com.troop.freedcam.sonyapi;
 
+import android.content.Context;
 import android.hardware.Camera;
+import android.util.Log;
 import android.view.SurfaceHolder;
 
 import com.troop.freedcam.i_camera.AbstractCameraHolder;
 import com.troop.freedcam.i_camera.I_CameraHolder;
+import com.troop.freedcam.sonyapi.sonystuff.ServerDevice;
+import com.troop.freedcam.sonyapi.sonystuff.SimpleCameraEventObserver;
+import com.troop.freedcam.sonyapi.sonystuff.SimpleRemoteApi;
+import com.troop.freedcam.sonyapi.sonystuff.SimpleStreamSurfaceView;
+import com.troop.freedcam.ui.MainActivity_v2;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Created by troop on 11.12.2014.
  */
 public class CameraHolderSony extends AbstractCameraHolder
 {
+    final static String TAG = CameraHolderSony.class.getSimpleName();
 
+    MainActivity_v2 context;
+
+    ServerDevice serverDevice;
+
+    private SimpleCameraEventObserver mEventObserver;
+
+    private SimpleCameraEventObserver.ChangeListener mEventListener;
+
+    private SimpleRemoteApi mRemoteApi;
+
+    private final Set<String> mAvailableCameraApiSet = new HashSet<String>();
+
+    private final Set<String> mSupportedApiSet = new HashSet<String>();
+    private SimpleStreamSurfaceView mLiveviewSurface;
+
+    public CameraHolderSony(Context context, SimpleStreamSurfaceView simpleStreamSurfaceView)
+    {
+        this.context = (MainActivity_v2)context;
+        this.mLiveviewSurface = simpleStreamSurfaceView;
+    }
+
+
+    public void setServerDevice(ServerDevice serverDevice)
+    {
+        this.serverDevice = serverDevice;
+        mRemoteApi = new SimpleRemoteApi(serverDevice);
+        mEventObserver = new SimpleCameraEventObserver(context, mRemoteApi);
+
+        mEventListener = new SimpleCameraEventObserver.ChangeListenerTmpl() {
+
+            @Override
+            public void onShootModeChanged(String shootMode) {
+
+            }
+
+            @Override
+            public void onCameraStatusChanged(String status) {
+
+            }
+
+            @Override
+            public void onApiListModified(List<String> apis) {
+
+                synchronized (mAvailableCameraApiSet) {
+                    mAvailableCameraApiSet.clear();
+                    for (String api : apis) {
+                        mAvailableCameraApiSet.add(api);
+                    }
+                    if (!mEventObserver.getLiveviewStatus() //
+                            && isCameraApiAvailable("startLiveview")) {
+                        if (mLiveviewSurface != null && !mLiveviewSurface.isStarted()) {
+                            startLiveview();
+                        }
+                    }
+                    if (isCameraApiAvailable("actZoom")) {
+
+
+                    } else {
+
+                    }
+                }
+            }
+
+            @Override
+            public void onZoomPositionChanged(int zoomPosition) {
+
+            }
+
+            @Override
+            public void onLiveviewStatusChanged(boolean status) {
+
+            }
+
+            @Override
+            public void onStorageIdChanged(String storageId) {
+
+            }
+        };
+
+    }
 
     @Override
     public boolean OpenCamera(int camera) {
@@ -38,15 +135,6 @@ public class CameraHolderSony extends AbstractCameraHolder
         return false;
     }
 
-    @Override
-    public boolean SetCameraParameters(Camera.Parameters parameters) {
-        return false;
-    }
-
-    @Override
-    public boolean SetSurface(SurfaceHolder texture) {
-        return false;
-    }
 
     @Override
     public void StartPreview() {
@@ -56,5 +144,76 @@ public class CameraHolderSony extends AbstractCameraHolder
     @Override
     public void StopPreview() {
 
+    }
+
+    /**
+     * Check if the specified API is available at present. This works correctly
+     * only for Camera API.
+     *
+     * @param apiName
+     * @return
+     */
+    private boolean isCameraApiAvailable(String apiName) {
+        boolean isAvailable = false;
+        synchronized (mAvailableCameraApiSet) {
+            isAvailable = mAvailableCameraApiSet.contains(apiName);
+        }
+        return isAvailable;
+    }
+
+    private void startLiveview() {
+        if (mLiveviewSurface == null) {
+            Log.w(TAG, "startLiveview mLiveviewSurface is null.");
+            return;
+        }
+        new Thread() {
+            @Override
+            public void run() {
+
+                try {
+                    JSONObject replyJson = null;
+                    replyJson = mRemoteApi.startLiveview();
+
+                    if (!SimpleRemoteApi.isErrorReply(replyJson)) {
+                        JSONArray resultsObj = replyJson.getJSONArray("result");
+                        if (1 <= resultsObj.length()) {
+                            // Obtain liveview URL from the result.
+                            final String liveviewUrl = resultsObj.getString(0);
+                            context.runOnUiThread(new Runnable() {
+
+                                @Override
+                                public void run() {
+                                    mLiveviewSurface.start(liveviewUrl, //
+                                            new SimpleStreamSurfaceView.StreamErrorListener() {
+
+                                                @Override
+                                                public void onError(StreamErrorReason reason) {
+                                                    stopLiveview();
+                                                }
+                                            });
+                                }
+                            });
+                        }
+                    }
+                } catch (IOException e) {
+                    Log.w(TAG, "startLiveview IOException: " + e.getMessage());
+                } catch (JSONException e) {
+                    Log.w(TAG, "startLiveview JSONException: " + e.getMessage());
+                }
+            }
+        }.start();
+    }
+
+    private void stopLiveview() {
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    mRemoteApi.stopLiveview();
+                } catch (IOException e) {
+                    Log.w(TAG, "stopLiveview IOException: " + e.getMessage());
+                }
+            }
+        }.start();
     }
 }
