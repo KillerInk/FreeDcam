51e42813a774ea3345f401b9dc1e1e903c8a15a7
troop <trooopiii@googlemail.com>
2014-12-23 16:31:03 +0100

    sony: working zoom slider wich sets the zoom correct  +-4%

--- app/src/main/java/com/troop/freedcam/sonyapi/parameters/manual/ZoomManualSony.java
+++ app/src/main/java/com/troop/freedcam/sonyapi/parameters/manual/ZoomManualSony.java
@@ -15,6 +15,8 @@ import java.io.IOException;
 public class ZoomManualSony extends BaseManualParameterSony
 {
     int currentzoomPos;
+    int zoomToSet;
+    private boolean isZooming = false;
 
     public ZoomManualSony(String MAX_TO_GET, String MIN_TO_GET, String CURRENT_TO_GET, ParameterHandlerSony parameterHandlerSony) {
         super(MAX_TO_GET, MIN_TO_GET, CURRENT_TO_GET, parameterHandlerSony);
@@ -73,24 +75,55 @@ public class ZoomManualSony extends BaseManualParameterSony
     @Override
     public void SetValue(int valueToSet)
     {
-        final String movement = "start";
-        String direction;
-        if (valueToSet < currentzoomPos)
-            direction = "out";
-        else
-            direction = "in";
-        currentzoomPos = valueToSet;
-        final String finaldirection = direction;
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    JSONObject object = ParameterHandler.mRemoteApi.actZoom(finaldirection, movement);
-                } catch (IOException e) {
-                    e.printStackTrace();
+
+        zoomToSet = valueToSet;
+        if (!isZooming)
+        {
+            isZooming = true;
+            final String movement = "1shot";
+            String direction;
+            if (valueToSet < currentzoomPos)
+                direction = "out";
+            else
+                direction = "in";
+            //currentzoomPos = valueToSet;
+            final String finaldirection = direction;
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        JSONObject object = ParameterHandler.mRemoteApi.actZoom(finaldirection, movement);
+                        isZooming = false;
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
-            }
-        }).start();
-        super.SetValue(valueToSet);
+            }).start();
+        }
+        //super.SetValue(valueToSet);
+    }
+
+    public void setZoomsHasChanged(int zoom)
+    {
+
+        currentzoomPos = zoom;
+        if(!checkIfIntIsInRange(zoomToSet, currentzoomPos))
+            SetValue(zoomToSet);
+        else
+            zoomToSet = currentzoomPos;
+        super.currentValueChanged(zoom);
+    }
+
+
+    private boolean checkIfIntIsInRange(int a, int b)
+    {
+        // 1 = 1
+        if (a == b)
+            return  true;
+            //1 = 3
+        else if (a - 4 <= b && a + 4 >= b)
+            return  true;
+        else
+            return false;
     }
 }
