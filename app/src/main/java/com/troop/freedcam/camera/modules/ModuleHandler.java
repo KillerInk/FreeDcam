fcc3d1fb9b0a7c2035b316a5a9fa27586fcd398a
troop <trooopiii@googlemail.com>
2015-01-08 20:43:00 +0100

    show wich module gets loaded to log

--- app/src/main/java/com/troop/freedcam/camera/modules/ModuleHandler.java
+++ app/src/main/java/com/troop/freedcam/camera/modules/ModuleHandler.java
@@ -1,5 +1,7 @@
 package com.troop.freedcam.camera.modules;
 
+import android.util.Log;
+
 import com.troop.freedcam.camera.BaseCameraHolder;
 import com.troop.freedcam.i_camera.AbstractCameraHolder;
 import com.troop.freedcam.i_camera.modules.AbstractModuleHandler;
@@ -30,34 +32,42 @@ public class ModuleHandler extends AbstractModuleHandler
         //splitting modules make the code foreach device cleaner
         if (DeviceUtils.isMediaTekDevice())
         {
+            Log.d(TAG, "load mtk picmodule");
             PictureModuleThl5000 thl5000 = new PictureModuleThl5000(this.cameraHolder, appSettingsManager, moduleEventHandler);
             moduleList.put(thl5000.ModuleName(), thl5000);
         }
         else if (DeviceUtils.isOmap())
         {
+            Log.d(TAG, "load omap picmodule");
             PictureModuleO3D omap = new PictureModuleO3D(this.cameraHolder,appSettingsManager,moduleEventHandler);
             moduleList.put(omap.ModuleName(),omap);
         }
         else //use default pictureModule
         {
+            Log.d(TAG, "load default picmodule");
             PictureModule pictureModule = new PictureModule(this.cameraHolder, appSettingsManager, moduleEventHandler);
             moduleList.put(pictureModule.ModuleName(), pictureModule);
         }
 
-        if (cameraHolder.hasLGFrameWork) {
+        if (cameraHolder.hasLGFrameWork)
+        {
+            Log.d(TAG, "load lg videomodule");
             VideoModuleG3 videoModuleG3 = new VideoModuleG3(this.cameraHolder, appSettingsManager, moduleEventHandler);
             moduleList.put(videoModuleG3.ModuleName(), videoModuleG3);
         }
         else if(cameraHolder.hasSamsungFrameWork) {
+            Log.d(TAG, "load samsung videomodule");
             VideoModuleSamsung videoModuleSamsung = new VideoModuleSamsung(this.cameraHolder, appSettingsManager, moduleEventHandler);
             moduleList.put(videoModuleSamsung.ModuleName(), videoModuleSamsung);
         }
         else
         {
+            Log.d(TAG, "load default videomodule");
             VideoModule videoModule = new VideoModule(this.cameraHolder, appSettingsManager, moduleEventHandler);
             moduleList.put(videoModule.ModuleName(), videoModule);
         }
 
+        Log.d(TAG, "load hdr videomodule");
         HdrModule hdrModule = new HdrModule(this.cameraHolder,appSettingsManager, moduleEventHandler);
         moduleList.put(hdrModule.ModuleName(), hdrModule);
 
