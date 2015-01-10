6c86a1199d5b8df1b8b7dbfc2fa9fe5c83d37099
troop <trooopiii@googlemail.com>
2015-01-10 10:44:03 +0100

    basic class for dng switch

---
+++ app/src/main/java/com/troop/freedcam/ui/menu/childs/ExpandAbleChildBoolean.java
@@ -0,0 +1,16 @@
+package com.troop.freedcam.ui.menu.childs;
+
+import android.content.Context;
+
+import com.troop.freedcam.ui.AppSettingsManager;
+import com.troop.freedcam.ui.menu.ExpandableGroup;
+
+/**
+ * Created by troop on 08.01.2015.
+ */
+public class ExpandAbleChildBoolean extends ExpandableChild
+{
+    public ExpandAbleChildBoolean(Context context, ExpandableGroup group, String name, AppSettingsManager appSettingsManager, String settingsname) {
+        super(context, group, name, appSettingsManager, settingsname);
+    }
+}
