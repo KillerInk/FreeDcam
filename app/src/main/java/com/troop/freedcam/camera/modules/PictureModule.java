7e2c8e24d3863901e60e56053447bf62b28c57c3
troop <trooopiii@googlemail.com>
2015-01-13 15:39:53 +0100

    add support to save yuv images

--- app/src/main/java/com/troop/freedcam/camera/modules/PictureModule.java
+++ app/src/main/java/com/troop/freedcam/camera/modules/PictureModule.java
@@ -29,7 +29,7 @@ public class PictureModule extends AbstractModule implements I_Callbacks.Picture
 
     private static String TAG = StringUtils.TAG + PictureModule.class.getSimpleName();
 
-    private String rawFormats = "bayer-mipi-10gbrg,bayer-mipi-10grbg,bayer-mipi-10rggb,bayer-mipi-10bggr,raw,bayer-qcom-10gbrg,bayer-qcom-10grbg,bayer-qcom-10rggb,bayer-qcom-10bggr,bayer-ideal-qcom-10grbg";
+    private String rawFormats = "bayer-mipi-10gbrg,bayer-mipi-10grbg,bayer-mipi-10rggb,bayer-mipi-10bggr,raw,bayer-qcom-10gbrg,bayer-qcom-10grbg,bayer-qcom-10rggb,bayer-qcom-10bggr,bayer-ideal-qcom-10grbg,bayer-ideal-qcom-10bggr";
     private String jpegFormat = "jpeg";
     private String jpsFormat = "jps";
 
@@ -245,6 +245,10 @@ public class PictureModule extends AbstractModule implements I_Callbacks.Picture
             else
                 return new File(s1 + "_" + pictureFormat + ".raw");
 
+        }
+        else if (pictureFormat.contains("yuv"))
+        {
+            return new File(s1 + "_" + pictureFormat + ".yuv");
         }
         else
         {
