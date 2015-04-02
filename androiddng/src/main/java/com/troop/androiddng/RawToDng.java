fb048d12f8f5ab0d169330de1bc6777cb49331aa
troop <trooopiii@googlemail.com>
2015-04-02 16:24:38 +0200

    add m9 blacklvl 64

--- androiddng/src/main/java/com/troop/androiddng/RawToDng.java
+++ androiddng/src/main/java/com/troop/androiddng/RawToDng.java
@@ -38,11 +38,11 @@ public class RawToDng
         //ElifeE7(                19906560, "Gionee Elife E7",        0,              g3_color1, g3_color2, g3_neutral, "grbg",4608,3456, true,   0),
         //OmniVision_OV5648(       6721536, "OmniVision_OV5648",      0,              g3_color1, g3_color2, g3_neutral, "grbg",2592,1944, true,   0),
         //looseraws
-        XperiaL(                10788864 , 64,  BGGR,3282,2448, false,  XperiaL_rowSize),
+        XperiaL(                10788864 , g3_blacklevel,  BGGR,3282,2448, false,  XperiaL_rowSize),
         OneSV(                6746112 , 0,  BGGR,2592,1944, false,  XperiaL_rowSize),
-        MT4G(                10782464 , 64,  RGGb,3282,2448, false,  XperiaL_rowSize),
-        M9Mipi(25677824 ,0,GRBG, 5388, 3752, true,0),
-        M9Qcom(27127808,0,GRBG, 5388, 3752, false,7230);
+        MT4G(                10782464 , g3_blacklevel,  RGGb,3282,2448, false,  XperiaL_rowSize),
+        M9Mipi(25677824 ,g3_blacklevel,GRBG, 5388, 3752, true,0),
+        M9Qcom(27127808,g3_blacklevel,GRBG, 5388, 3752, false,7230);
         //OmniVision_OV5648_1(    6721536,  "OmniVision_OV5648_1",    0,              g3_color1, g3_color2, g3_neutral, "grbg",2592,1944, false,  0),
         //HTCOneSV(               6746112,  "HTCOneSV",               0,              g3_color1, g3_color2, g3_neutral, "grbg",2592,1944, false,  0),
         //HTC_MyTouch_4G_Slide(   10782464, "HTC_MyTouch_4G_Slide",   0,              g3_color1, g3_color2, g3_neutral, "grbg",3282,2448, false,  0);
