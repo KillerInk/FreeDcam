cf8edd3f13e2270332ae75ea4d837819a34eb078
troop <trooopiii@googlemail.com>
2015-06-13 13:41:02 +0200

    adding Cameraswitch, modeswitch and exit to ui with no functuin

---
+++ themesample/src/main/java/troop/com/themesample/views/UiSettingsChildCameraSwitch.java
@@ -0,0 +1,17 @@
+package troop.com.themesample.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+/**
+ * Created by troop on 13.06.2015.
+ */
+public class UiSettingsChildCameraSwitch extends UiSettingsChild {
+    public UiSettingsChildCameraSwitch(Context context) {
+        super(context);
+    }
+
+    public UiSettingsChildCameraSwitch(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
---
+++ themesample/src/main/java/troop/com/themesample/views/UiSettingsChildModeSwitch.java
@@ -0,0 +1,17 @@
+package troop.com.themesample.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+/**
+ * Created by troop on 13.06.2015.
+ */
+public class UiSettingsChildModeSwitch extends UiSettingsChild {
+    public UiSettingsChildModeSwitch(Context context) {
+        super(context);
+    }
+
+    public UiSettingsChildModeSwitch(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
