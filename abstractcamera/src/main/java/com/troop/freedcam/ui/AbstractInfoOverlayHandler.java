f58cf89b7720c89541e553ed14e34567db74313c
troop <trooopiii@googlemail.com>
2015-06-14 11:57:33 +0200

    moving parameters reading into AbstractInfoOverlayHandler that it can easy extended for settting values to textviews in the ui

---
+++ abstractcamera/src/main/java/com/troop/freedcam/ui/AbstractInfoOverlayHandler.java
@@ -0,0 +1,187 @@
+package com.troop.freedcam.ui;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.media.CamcorderProfile;
+import android.os.BatteryManager;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.StatFs;
+
+import com.troop.freedcam.i_camera.AbstractCameraUiWrapper;
+import com.troop.freedcam.i_camera.modules.AbstractModuleHandler;
+import com.troop.freedcam.i_camera.modules.I_ModuleEvent;
+import com.troop.freedcam.utils.StringUtils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by troop on 14.06.2015.
+ */
+public abstract class AbstractInfoOverlayHandler implements I_ModuleEvent
+{
+    protected AppSettingsManager appSettingsManager;
+    Handler handler;
+    protected AbstractCameraUiWrapper cameraUiWrapper;
+    boolean started = false;
+    Context context;
+
+    protected String batteryLevel;
+    BatteryBroadCastListner batteryBroadCastListner;
+
+    SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+    protected String timeString;
+
+    //this holds the format for video or picture
+    protected String format;
+    //this holds the size for video/picture
+    protected String size;
+
+    protected String storageSpace;
+
+    public AbstractInfoOverlayHandler(Context context, AppSettingsManager appSettingsManager)
+    {
+        this.appSettingsManager = appSettingsManager;
+        handler = new Handler();
+        batteryBroadCastListner = new BatteryBroadCastListner();
+    }
+
+    public void setCameraUIWrapper(AbstractCameraUiWrapper cameraUIWrapper)
+    {
+        this.cameraUiWrapper = cameraUIWrapper;
+        cameraUIWrapper.moduleHandler.moduleEventHandler.addListner(this);
+    }
+
+    @Override
+    public String ModuleChanged(String module) {
+        return null;
+    }
+
+    private void startLooperThread()
+    {
+        if (started)
+            handler.postDelayed(runner, 1000);
+    }
+
+    public void StartUpdating()
+    {
+        started = true;
+        context.registerReceiver(batteryBroadCastListner, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+        startLooperThread();
+    }
+
+    public void StopUpdating()
+    {
+        started = false;
+        handler.removeCallbacks(runner);
+        context.unregisterReceiver(batteryBroadCastListner);
+
+    }
+
+    class BatteryBroadCastListner extends BroadcastReceiver
+    {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            batteryLevel = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0)+"%";
+        }
+    }
+
+    Runnable runner = new Runnable() {
+        @Override
+        public void run()
+        {
+            timeString = dateFormat.format(new Date());
+            getFormat();
+            getStorageSpace();
+            UpdateViews();
+
+
+            startLooperThread();
+
+        }
+    };
+
+    protected void UpdateViews()
+    {
+
+    }
+
+    private void getFormat()
+    {
+        if (cameraUiWrapper.moduleHandler.GetCurrentModuleName().equals(AbstractModuleHandler.MODULE_VIDEO))
+        {
+            format = "H264";
+            size = appSettingsManager.getString(AppSettingsManager.SETTING_VIDEPROFILE);
+        }
+        else
+        {
+            if (cameraUiWrapper.camParametersHandler.PictureFormat != null)
+                format = cameraUiWrapper.camParametersHandler.PictureFormat.GetValue();
+            else
+                format = "";
+            if (cameraUiWrapper.camParametersHandler.PictureSize != null)
+                size = cameraUiWrapper.camParametersHandler.PictureSize.GetValue();
+            else
+                size = "";
+        }
+    }
+
+    public void getStorageSpace()
+    {
+        try {
+            //Storage.setText(StringUtils.readableFileSize(Environment.getExternalStorageDirectory().getUsableSpace()));
+
+            //defcomg was here 24/01/2015
+            if(!cameraUiWrapper.moduleHandler.GetCurrentModuleName().equals(AbstractModuleHandler.MODULE_VIDEO))
+                storageSpace = Avail4PIC();
+            else
+                storageSpace =StringUtils.readableFileSize(SDspace());
+        }
+        catch (Exception ex)
+        {
+            storageSpace = "";
+        }
+
+
+    }
+
+    private  String Avail4PIC()
+    {
+        // double calc;
+        long done;
+        done = (long) Calc();
+        long a = SDspace() / done;
+        return  a + " left";
+    }
+    private double Calc()
+    {
+        double calc;
+        String res [] = appSettingsManager.getString(AppSettingsManager.SETTING_PICTURESIZE).split("x");
+
+        if(appSettingsManager.getString(AppSettingsManager.SETTING_PICTUREFORMAT).contains("bayer"))
+        {
+            if (appSettingsManager.getString(AppSettingsManager.SETTING_DNG).equals("true"))
+                return calc = Integer.parseInt(res[0]) * 2 *Integer.parseInt(res[1]) *1.2;
+            else
+                return calc = Integer.parseInt(res[0]) *Integer.parseInt(res[1]) *1.26;
+        }
+        else
+            return calc = Integer.parseInt(res[0]) *Integer.parseInt(res[1]) *1.2;
+    }
+
+    private static long SDspace()
+    {
+        StatFs stat = new StatFs(Environment.getExternalStorageDirectory().getPath());
+        stat.restat(Environment.getExternalStorageDirectory().getPath());
+        long bytesAvailable = Environment.getExternalStorageDirectory().getUsableSpace();
+        return bytesAvailable;
+    }
+
+
+
+
+}
--- themeclassic/src/main/java/com/troop/freedcam/ui/menu/themes/classic/InfoOverlayHandler.java
+++ themeclassic/src/main/java/com/troop/freedcam/ui/menu/themes/classic/InfoOverlayHandler.java
@@ -20,6 +20,7 @@ import com.troop.freedcam.camera.modules.ModuleHandler;
 import com.troop.freedcam.camera.parameters.modes.VideoProfilesParameter;
 import com.troop.freedcam.i_camera.AbstractCameraUiWrapper;
 import com.troop.freedcam.i_camera.modules.I_ModuleEvent;
+import com.troop.freedcam.ui.AbstractInfoOverlayHandler;
 import com.troop.freedcam.ui.AppSettingsManager;
 import com.troop.freedcam.ui.menu.themes.R;
 import com.troop.freedcam.utils.StringUtils;
@@ -31,25 +32,20 @@ import java.util.Date;
 /**
  * Created by troop on 23.01.2015.
  */
-public class InfoOverlayHandler extends BroadcastReceiver implements I_ModuleEvent {
+public class InfoOverlayHandler extends AbstractInfoOverlayHandler implements I_ModuleEvent {
     //troopii was here and cleaned up^^
     private final View view;
-    private final AppSettingsManager appSettingsManager;
     TextView batteryLoad;
     TextView Storage;
     TextView pictureSize;
     TextView pictureFormat;
     TextView time;
-    boolean started = false;
-    AbstractCameraUiWrapper cameraUiWrapper;
-    Handler handler = new Handler();
-    SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
     Typeface font;
 
     public InfoOverlayHandler(View view, AppSettingsManager appSettingsManager)
     {
+        super(view.getContext(), appSettingsManager);
         this.view = view;
-        this.appSettingsManager = appSettingsManager;
         batteryLoad = (TextView)view.findViewById(R.id.txtViewBattLevel);
 
         Storage = (TextView)view.findViewById(R.id.txtViewRemainingStorage);
@@ -79,114 +75,13 @@ public class InfoOverlayHandler extends BroadcastReceiver implements I_ModuleEve
         }
     }
 
-    public void setCameraUIWrapper(AbstractCameraUiWrapper cameraUIWrapper)
-    {
-        this.cameraUiWrapper = cameraUIWrapper;
-        cameraUIWrapper.moduleHandler.moduleEventHandler.addListner(this);
-    }
-
-    //i think a handler would be better with postdelayed
-    //or more better would be to listen to the onvaluechanged event from parameters
-    private void startLooperThread()
-    {
-        if (started)
-            handler.postDelayed(runner, 1000);
-    }
-
-    public void StartUpdating()
-    {
-        setThemeFonts();
-        started = true;
-        view.getContext().registerReceiver(this, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
-        startLooperThread();
-    }
-
-    public void StopUpdating()
-    {
-        started = false;
-        handler.removeCallbacks(runner);
-        view.getContext().unregisterReceiver(this);
-
-    }
-
-    Runnable runner = new Runnable() {
-        @Override
-        public void run()
-        {
-            time.setText(dateFormat.format(new Date()));
-            if (true)
-            {
-                if (cameraUiWrapper.moduleHandler.GetCurrentModuleName().equals(ModuleHandler.MODULE_VIDEO) || cameraUiWrapper.moduleHandler.GetCurrentModuleName().equals(ModuleHandler.MODULE_LONGEXPO)) {
-
-                    if (!cameraUiWrapper.moduleHandler.GetCurrentModuleName().equals(ModuleHandler.MODULE_LONGEXPO)) {
-                        pictureFormat.setText("H264");
-                        pictureSize.setText(appSettingsManager.getString(AppSettingsManager.SETTING_VIDEPROFILE));
-                    }
-                }
-                else
-                {
-                    setPictureSize();
-
-                    setPictureFormat();
-                }
-                trySet();
-            }
-            startLooperThread();
-
-        }
-    };
-
-    private void setPictureSize()
-    {
-        if (cameraUiWrapper instanceof CameraUiWrapper) {
-            if (cameraUiWrapper.camParametersHandler.PictureSize != null)
-            {
-                String RESRAY[] = cameraUiWrapper.camParametersHandler.PictureSize.GetValue().split("x");
-                if (RESRAY.length < 2)
-                    return;
-                double mp = (Integer.parseInt(RESRAY[0]) * Integer.parseInt(RESRAY[1])) / 1000000;
-                DecimalFormat decimalFormat = new DecimalFormat("#.##");
-                pictureSize.setText(String.valueOf(decimalFormat.format(mp)) + "MP");
-            }
-        }
-        else if (cameraUiWrapper.camParametersHandler.PictureSize != null)
-            pictureSize.setText(cameraUiWrapper.camParametersHandler.PictureSize.GetValue());
-    }
-
-    private void setPictureFormat()
-    {
-        if (cameraUiWrapper instanceof CameraUiWrapper) {
-            if (appSettingsManager.getString(AppSettingsManager.SETTING_PICTUREFORMAT).contains("bayer") || appSettingsManager.getString(AppSettingsManager.SETTING_PICTUREFORMAT).contains("raw")) {
-                if (appSettingsManager.getString(AppSettingsManager.SETTING_DNG).equals("true"))
-                    pictureFormat.setText("DNG");
-                else
-                    pictureFormat.setText("RAW");
-            } else
-                pictureFormat.setText(appSettingsManager.getString(AppSettingsManager.SETTING_PICTUREFORMAT));
-        }
-        else if (cameraUiWrapper.camParametersHandler.PictureFormat != null)
-            pictureFormat.setText(cameraUiWrapper.camParametersHandler.PictureFormat.GetValue());
-    }
-
-    //defcomg was here this should go into some handler class that handles module change
-    public void trySet()
-    {
-
-        try {
-            //Storage.setText(StringUtils.readableFileSize(Environment.getExternalStorageDirectory().getUsableSpace()));
-
-            //defcomg was here 24/01/2015
-            if(!cameraUiWrapper.moduleHandler.GetCurrentModuleName().equals(ModuleHandler.MODULE_VIDEO))
-                Storage.setText(Avail4PIC());
-            else
-                Storage.setText(StringUtils.readableFileSize(SDspace()));
-        }
-        catch (Exception ex)
-        {
-            Storage.setText("error");
-        }
-
-
+    @Override
+    protected void UpdateViews() {
+        batteryLoad.setText(batteryLevel);
+        pictureFormat.setText(format);
+        pictureSize.setText(size);
+        time.setText(timeString);
+        Storage.setText(storageSpace);
     }
 
     private void setThemeFonts() {
@@ -224,62 +119,9 @@ public class InfoOverlayHandler extends BroadcastReceiver implements I_ModuleEve
         }
     }
 
-    private String Avail4Video()
-    {
-        int ByteDiv = 1048576;
-        int BitDiv = 8;
-
-        System.out.println("Video "+SDspace());
-
-        long MBytes = SDspace() / ByteDiv;
-
-        System.out.println("Video "+MBytes);
-
-        String profile = appSettingsManager.getString(AppSettingsManager.SETTING_VIDEPROFILE);
-        VideoProfilesParameter videoProfilesParameter = (VideoProfilesParameter)cameraUiWrapper.camParametersHandler.VideoProfiles;
 
 
-        CamcorderProfile prof = videoProfilesParameter.GetCameraProfile(profile);
-        System.out.println("Video "+prof.videoBitRate);
 
-        long Bitt = (prof.videoBitRate /1000)/8;
-
-        System.out.println("Video "+Bitt);
-
-        return String.valueOf(MBytes/Bitt);
-    }
-
-    private  String Avail4PIC()
-    {
-        // double calc;
-        long done;
-        done = (long) Calc();
-        long a = SDspace() / done;
-        return  a + " left";
-    }
-    private double Calc()
-    {
-        double calc;
-        String res [] = appSettingsManager.getString(AppSettingsManager.SETTING_PICTURESIZE).split("x");
-
-        if(appSettingsManager.getString(AppSettingsManager.SETTING_PICTUREFORMAT).contains("bayer"))
-        {
-            if (appSettingsManager.getString(AppSettingsManager.SETTING_DNG).equals("true"))
-                return calc = Integer.parseInt(res[0]) * 2 *Integer.parseInt(res[1]) *1.2;
-            else
-                return calc = Integer.parseInt(res[0]) *Integer.parseInt(res[1]) *1.26;
-        }
-        else
-            return calc = Integer.parseInt(res[0]) *Integer.parseInt(res[1]) *1.2;
-    }
-
-    private static long SDspace()
-    {
-        StatFs stat = new StatFs(Environment.getExternalStorageDirectory().getPath());
-        stat.restat(Environment.getExternalStorageDirectory().getPath());
-        long bytesAvailable = Environment.getExternalStorageDirectory().getUsableSpace();
-        return bytesAvailable;
-    }
 
     @Override
     public String ModuleChanged(String module) {
@@ -288,12 +130,6 @@ public class InfoOverlayHandler extends BroadcastReceiver implements I_ModuleEve
         return null;
 
 
-    }
-
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
-        batteryLoad.setText(String.valueOf(level) + "%");
     }
 
     //End defcomg
