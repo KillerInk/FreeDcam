1baa5028aac1c63a598b8f8853f90f8c366e54a1
troop <trooopiii@googlemail.com>
2015-07-19 19:39:49 +0200

    add picturesize

--- themesample/src/main/java/troop/com/themesample/subfragments/LeftMenuFragment.java
+++ themesample/src/main/java/troop/com/themesample/subfragments/LeftMenuFragment.java
@@ -2,6 +2,7 @@ package troop.com.themesample.subfragments;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -20,6 +21,7 @@ public class LeftMenuFragment extends AbstractFragment  implements Interfaces.I_
 {
     MenuItemTheme themeItem;
     MenuItemBayerFormat bayerFormatItem;
+    troop.com.themesample.views.menu.MenuItem pictureSize;
 
     Interfaces.I_MenuItemClick onMenuItemClick;
 
@@ -42,6 +44,11 @@ public class LeftMenuFragment extends AbstractFragment  implements Interfaces.I_
         bayerFormatItem.SetStuff(i_activity,appSettingsManager,"");
         bayerFormatItem.SetParameter(wrapper.camParametersHandler.PictureFormat);
         bayerFormatItem.SetMenuItemListner(this);
+
+        pictureSize = (troop.com.themesample.views.menu.MenuItem)view.findViewById(R.id.MenuItemPicSize);
+        pictureSize.SetStuff(i_activity,appSettingsManager, AppSettingsManager.SETTING_PICTURESIZE);
+        pictureSize.SetParameter(wrapper.camParametersHandler.PictureSize);
+        pictureSize.SetMenuItemListner(this);
     }
 
     public void SetMenuItemClickListner(Interfaces.I_MenuItemClick menuItemClick)
