24cd5767960afafb94f9cf4df05c71f1006e561f
troop <trooopiii@googlemail.com>
2015-08-16 17:39:52 +0200

    Revert "changed g3 to use Build.Device instead of Build.Model. that should make it unessescary to add all build.models foreach device"
    This reverts commit 350dec96bf93e85710bdc30e6875ee7bb386fe57.

--- abstractcamera/src/main/java/com/troop/freedcam/utils/DeviceUtils.java
+++ abstractcamera/src/main/java/com/troop/freedcam/utils/DeviceUtils.java
@@ -83,11 +83,10 @@ public class DeviceUtils
     public static boolean isHtc_One_XL() { return Build.MODEL.toLowerCase().contains("one xl");}
 
 
-    //public static String G3Models = "LG-D855,LGLS990,LG VS985,LG-D851";
-    public static String G3Device = "g3";
+    public static String G3Models = "LG-D855,LGLS990,LG VS985,LG-D851";
     public static boolean isLG_G3()
     {
-        return G3Device.toLowerCase().equals(Build.DEVICE);
+        return G3Models.contains(Build.MODEL);
     }
 
     public static boolean getBoolFromInt(int i)
