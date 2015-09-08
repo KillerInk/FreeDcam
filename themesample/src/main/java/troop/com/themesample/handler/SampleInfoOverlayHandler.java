dc956c57ec67ee0e71b24bea544759c3fd8cb705
troop <trooopiii@googlemail.com>
2015-09-08 13:38:24 +0200

    add build.model , dng support and app version to infooverlay

--- themesample/src/main/java/troop/com/themesample/handler/SampleInfoOverlayHandler.java
+++ themesample/src/main/java/troop/com/themesample/handler/SampleInfoOverlayHandler.java
@@ -1,11 +1,15 @@
 package troop.com.themesample.handler;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.os.Build;
 import android.view.View;
 import android.widget.TextView;
 
 import com.troop.freedcam.ui.AbstractInfoOverlayHandler;
 import com.troop.freedcam.ui.AppSettingsManager;
+import com.troop.freedcam.utils.DeviceUtils;
 
 import troop.com.themesample.R;
 
@@ -19,6 +23,9 @@ public class SampleInfoOverlayHandler extends AbstractInfoOverlayHandler
     TextView tformat;
     TextView tTime;
     TextView tStorage;
+    TextView tdngsupported;
+    TextView tbuidlmodel;
+    TextView tappversion;
     public SampleInfoOverlayHandler(View view, AppSettingsManager appSettingsManager)
     {
         super(view.getContext(), appSettingsManager);
@@ -27,6 +34,25 @@ public class SampleInfoOverlayHandler extends AbstractInfoOverlayHandler
         tformat = (TextView)view.findViewById(R.id.textView_format);
         tTime = (TextView)view.findViewById(R.id.textView_time);
         tStorage = (TextView)view.findViewById(R.id.textView_storage);
+        tdngsupported=(TextView)view.findViewById(R.id.textView_dngsupported);
+        if (appSettingsManager.getCamApi().equals(AppSettingsManager.API_1))
+        {
+            tdngsupported.setText("DNG:" + DeviceUtils.isCamera1DNGSupportedDevice());
+            if (DeviceUtils.isCamera1DNGSupportedDevice())
+                tdngsupported.setTextColor(Color.GREEN);
+            else
+                tdngsupported.setTextColor(Color.RED);
+        }
+        else tdngsupported.setVisibility(View.GONE);
+        tbuidlmodel = (TextView)view.findViewById(R.id.textView_buildmodel);
+        tbuidlmodel.setText(Build.MODEL);
+        tappversion = (TextView)view.findViewById(R.id.textView_appversion);
+        try {
+            tappversion.setText(appSettingsManager.context.getPackageManager()
+                    .getPackageInfo(appSettingsManager.context.getPackageName(), 0).versionName);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
