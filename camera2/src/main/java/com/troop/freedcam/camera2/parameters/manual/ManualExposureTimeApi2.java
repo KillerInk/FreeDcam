d21c750e2348acd815fdf40b955eb24894f953f4
troop <trooopiii@googlemail.com>
2015-09-17 16:27:51 +0200

    move ShutterValues stuff from camera2 into abstractcamera to use it in other apis

--- camera2/src/main/java/com/troop/freedcam/camera2/parameters/manual/ManualExposureTimeApi2.java
+++ camera2/src/main/java/com/troop/freedcam/camera2/parameters/manual/ManualExposureTimeApi2.java
@@ -26,24 +26,12 @@ public class ManualExposureTimeApi2 extends AbstractManualParameter implements A
     BaseCameraHolderApi2 cameraHolder;
     boolean canSet = false;
     private boolean isSupported = false;
-
-    public static String ShutterValues = "Auto,1/90000,1/75000,1/50000,1/45000,1/30000,1/20000,1/12000,1/10000"+
-            ",1/8000,1/6400,1/5000,1/4000,1/3200,1/2500,1/2000,1/1600,1/1250,1/1000"+
-            ",1/800,1/700,1/600,1/500,1/400,1/300,1/200,1/125,1/100,1/85,1/75,1/65"+
-            ",1/55,1/45,1/35,1/25,1/20,1/15,1/13,1/10,1/9,1/8,1/7,1/6,1/5,1/4,1/3,1/2,0.8"+
-            ",1.0,1.2,1.4,1.5,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0,11.0,12.0,13.0,14.0"+
-            ",15.0,16.0,17.0,18.0,19.0,20.0,21.0,22.0,23.0,24.0,25.0,26.0,27.0,28.0,29.0"+
-            ",30.0,31.0,32.0,33.0,35.0,36.0,37.0,38.0,39.0,40.0,41.0,42.0,43.0,44,45.0,46.0"+
-            ",47.0,48.0,49.0,50.0,51.0,52.0,53.0,54.0,55.0,56.0,57.0,58.0,59.0,60.0,120.0,240.0";
-
-    String shutterValues[];
     String usedShutterValues[];
 
     public ManualExposureTimeApi2(ParameterHandlerApi2 camParametersHandler, BaseCameraHolderApi2 cameraHolder) {
         super(camParametersHandler);
         this.cameraHolder = cameraHolder;
         this.camParametersHandler = camParametersHandler;
-        shutterValues = ShutterValues.split(",");
         try {
             findMinMaxValue();
         }
@@ -51,46 +39,15 @@ public class ManualExposureTimeApi2 extends AbstractManualParameter implements A
         {
             this.isSupported = false;
         }
-
     }
 
     int current = 0;
-    int min = 0;
-    int max =0;
 
     private void findMinMaxValue()
     {
         int millimax = (cameraHolder.characteristics.get(CameraCharacteristics.SENSOR_INFO_EXPOSURE_TIME_RANGE).getUpper()).intValue() / 1000;
         int millimin = (cameraHolder.characteristics.get(CameraCharacteristics.SENSOR_INFO_EXPOSURE_TIME_RANGE).getLower()).intValue() / 1000;
-        boolean foundmin = false, foundmax = false;
-        ArrayList<String> tmp = new ArrayList<>();
-        for (int i = 1; i< shutterValues.length; i++ )
-        {
-            String s = shutterValues[i];
-
-            float a;
-            if (s.contains("/")) {
-                String split[] = s.split("/");
-                a =(Float.parseFloat(split[0]) / Float.parseFloat(split[1])*1000000f);
-            }
-            else
-                a = (Float.parseFloat(s)*1000000f);
-
-            if (a>= millimin && a <= millimax)
-                tmp.add(s);
-            if (a >= millimin && !foundmin)
-            {
-                foundmin = true;
-                min = i;
-            }
-            if (a > millimax && !foundmax)
-            {
-                foundmax = true;
-                max = i-1;
-            }
-            if (foundmax && foundmin)
-                usedShutterValues = tmp.toArray(new String[tmp.size()]);
-        }
+        usedShutterValues = StringUtils.getSupportedShutterValues(millimin, millimax);
     }
 
     @Override
@@ -121,21 +78,6 @@ public class ManualExposureTimeApi2 extends AbstractManualParameter implements A
     }
 
 
-
-    public String getSECONDSasString (long time)
-    {
-        double mili = time /1000000  ;
-        double sec =  mili / 1000;
-        return sec +"";
-    }
-
-    public int getMilliSECONDS (long time)
-    {
-        double mili = time /1000000  ;
-
-        return (int)mili;
-    }
-
     @Override
     public String[] getStringValues() {
         return usedShutterValues;
@@ -145,16 +87,8 @@ public class ManualExposureTimeApi2 extends AbstractManualParameter implements A
     @Override
     public void SetValue(int valueToSet)
     {
-        String s = usedShutterValues[valueToSet];
         current = valueToSet;
-        float a;
-        if (s.contains("/")) {
-            String split[] = s.split("/");
-            a =(Float.parseFloat(split[0]) / Float.parseFloat(split[1])*1000000f);
-        }
-        else
-            a = (Float.parseFloat(s)*1000000f);
-        cameraHolder.mPreviewRequestBuilder.set(CaptureRequest.SENSOR_EXPOSURE_TIME, (long)(a * 1000f));
+        cameraHolder.mPreviewRequestBuilder.set(CaptureRequest.SENSOR_EXPOSURE_TIME, (long)(StringUtils.getMilliSecondStringFromShutterString(usedShutterValues[valueToSet]) * 1000f));
         try {
             cameraHolder.mCaptureSession.setRepeatingRequest(cameraHolder.mPreviewRequestBuilder.build(), cameraHolder.mCaptureCallback,
                     null);
