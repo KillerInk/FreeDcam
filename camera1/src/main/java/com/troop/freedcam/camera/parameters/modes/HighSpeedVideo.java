795ff6b38f2c641b483b44b7785cbdcd2fc7a7dc
defcomk <defcomk@gmail.com>
2015-09-22 22:36:48 +0200

    Add to menuQualcomm HFR Mode Selection

---
+++ camera1/src/main/java/com/troop/freedcam/camera/parameters/modes/HighSpeedVideo.java
@@ -0,0 +1,7 @@
+package com.troop.freedcam.camera.parameters.modes;
+
+/**
+ * Created by GeorgeKiarie on 9/22/2015.
+ */
+public class HighSpeedVideo {
+}
---
+++ themesample/src/main/java/troop/com/themesample/views/menu/MenuItemHighSpeedVideo.java
@@ -0,0 +1,7 @@
+package troop.com.themesample.views.menu;
+
+/**
+ * Created by GeorgeKiarie on 9/22/2015.
+ */
+public class MenuItemHighSpeedVideo {
+}
