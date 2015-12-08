c4b319d9dc4dbde40eb9e409cb230000203a361a
troop <trooopiii@googlemail.com>
2015-12-08 16:37:49 +0100

    introduce new manual_fragment_rotatingseekbar

---
+++ themesample/src/main/java/troop/com/themesample/subfragments/ManualFragmentRotatingSeekbar.java
@@ -0,0 +1,213 @@
+package troop.com.themesample.subfragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.SeekBar;
+
+import com.troop.freedcam.i_camera.AbstractCameraUiWrapper;
+import com.troop.freedcam.i_camera.parameters.AbstractManualParameter;
+import com.troop.freedcam.i_camera.parameters.I_ParametersLoaded;
+import com.troop.freedcam.ui.AbstractFragment;
+import com.troop.freedcam.ui.AppSettingsManager;
+import com.troop.freedcam.ui.I_Activity;
+
+import troop.com.themesample.R;
+import troop.com.themesample.views.ManualButton;
+import troop.com.views.RotatingSeekbar;
+
+/**
+ * Created by troop on 08.12.2015.
+ */
+public class ManualFragmentRotatingSeekbar extends AbstractFragment implements I_ParametersLoaded, SeekBar.OnSeekBarChangeListener
+{
+
+    RotatingSeekbar seekbar;
+    ManualButton mf;
+    ManualButton iso;
+    ManualButton shutter;
+    ManualButton aperture;
+    ManualButton exposure;
+    ManualButton brightness;
+    ManualButton burst;
+    ManualButton wb;
+    ManualButton contrast;
+    ManualButton saturation;
+    ManualButton sharpness;
+    ManualButton programshift;
+    ManualButton zoom;
+    ManualButton skintone;
+    ManualButton fx;
+    ManualButton convergence;
+
+    ManualButton currentButton;
+
+
+    //#################VIEW STUFF##############################
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        View view = inflater.inflate(R.layout.manual_fragment_rotatingseekbar, container, false);
+        seekbar = (RotatingSeekbar)view.findViewById(R.id.seekbar);
+
+        mf = (ManualButton)view.findViewById(R.id.manual_mf);
+        mf.SetStuff(appSettingsManager, AppSettingsManager.MF);
+        mf.setOnClickListener(manualButtonClickListner);
+
+        iso = (ManualButton)view.findViewById(R.id.manual_iso);
+        iso.SetStuff(appSettingsManager, AppSettingsManager.MISO);
+        iso.setOnClickListener(manualButtonClickListner);
+
+        shutter = (ManualButton)view.findViewById(R.id.manual_shutter);
+        shutter.SetStuff(appSettingsManager, AppSettingsManager.MSHUTTERSPEED);
+        shutter.setOnClickListener(manualButtonClickListner);
+
+        aperture = (ManualButton)view.findViewById(R.id.manual_aperture);
+        aperture.SetStuff(appSettingsManager, "");
+        aperture.setOnClickListener(manualButtonClickListner);
+
+        exposure = (ManualButton)view.findViewById(R.id.manual_exposure);
+        exposure.SetStuff(appSettingsManager, AppSettingsManager.MEXPOSURE);
+        exposure.setOnClickListener(manualButtonClickListner);
+
+        brightness = (ManualButton)view.findViewById(R.id.manual_brightness);
+        brightness.SetStuff(appSettingsManager, AppSettingsManager.MBRIGHTNESS);
+        brightness.setOnClickListener(manualButtonClickListner);
+
+        burst = (ManualButton)view.findViewById(R.id.manual_burst);
+        burst.SetStuff(appSettingsManager, "");
+        burst.setOnClickListener(manualButtonClickListner);
+
+        wb = (ManualButton)view.findViewById(R.id.manual_wb);
+        wb.SetStuff(appSettingsManager, AppSettingsManager.MCCT);
+        wb.setOnClickListener(manualButtonClickListner);
+
+        contrast = (ManualButton)view.findViewById(R.id.manual_contrast);
+        contrast.SetStuff(appSettingsManager, AppSettingsManager.MCONTRAST);
+        contrast.setOnClickListener(manualButtonClickListner);
+
+        saturation = (ManualButton)view.findViewById(R.id.manual_saturation);
+        saturation.SetStuff(appSettingsManager, AppSettingsManager.MSATURATION);
+        saturation.setOnClickListener(manualButtonClickListner);
+
+        sharpness = (ManualButton)view.findViewById(R.id.manual_sharpness);
+        sharpness.SetStuff(appSettingsManager, AppSettingsManager.MSHARPNESS);
+        sharpness.setOnClickListener(manualButtonClickListner);
+
+        programshift = (ManualButton)view.findViewById(R.id.manual_program_shift);
+        programshift.SetStuff(appSettingsManager, "");
+        programshift.setOnClickListener(manualButtonClickListner);
+
+        zoom = (ManualButton)view.findViewById(R.id.manual_zoom);
+        zoom.SetStuff(appSettingsManager, "");
+        zoom.setOnClickListener(manualButtonClickListner);
+
+        skintone = (ManualButton)view.findViewById(R.id.manual_skintone);
+        skintone.SetStuff(appSettingsManager, "");
+        skintone.setOnClickListener(manualButtonClickListner);
+
+        fx  = (ManualButton)view.findViewById(R.id.manual_fx);
+        fx.SetStuff(appSettingsManager, "");
+        fx.setOnClickListener(manualButtonClickListner);
+
+        convergence = (ManualButton)view.findViewById(R.id.manual_convergence);
+        convergence.SetStuff(appSettingsManager, AppSettingsManager.MCONVERGENCE);
+        convergence.setOnClickListener(manualButtonClickListner);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        if (wrapper != null)
+            setWrapper();
+    }
+
+
+
+    @Override
+    public void SetCameraUIWrapper(AbstractCameraUiWrapper wrapper) {
+        super.SetCameraUIWrapper(wrapper);
+        try {
+            wrapper.camParametersHandler.ParametersEventHandler.AddParametersLoadedListner(this);
+            setWrapper();
+        }
+        catch (NullPointerException ex)
+        {
+            ex.printStackTrace();
+        }
+
+
+    }
+
+    @Override
+    public void SetStuff(AppSettingsManager appSettingsManager, I_Activity i_activity) {
+        super.SetStuff(appSettingsManager, i_activity);
+    }
+
+    private void setWrapper() {
+        contrast.SetAbstractManualParameter(wrapper.camParametersHandler.ManualContrast);
+        burst.SetAbstractManualParameter(wrapper.camParametersHandler.Burst);
+        brightness.SetAbstractManualParameter(wrapper.camParametersHandler.ManualBrightness);
+        wb.SetAbstractManualParameter(wrapper.camParametersHandler.CCT);
+        convergence.SetAbstractManualParameter(wrapper.camParametersHandler.ManualConvergence);
+        exposure.SetAbstractManualParameter(wrapper.camParametersHandler.ManualExposure);
+        fx.SetAbstractManualParameter(wrapper.camParametersHandler.FX);
+        mf.SetAbstractManualParameter(wrapper.camParametersHandler.ManualFocus);
+        saturation.SetAbstractManualParameter(wrapper.camParametersHandler.ManualSaturation);
+        sharpness.SetAbstractManualParameter(wrapper.camParametersHandler.ManualSharpness);
+        shutter.SetAbstractManualParameter(wrapper.camParametersHandler.ManualShutter);
+        iso.SetAbstractManualParameter(wrapper.camParametersHandler.ISOManual);
+        zoom.SetAbstractManualParameter(wrapper.camParametersHandler.Zoom);
+        aperture.SetAbstractManualParameter(wrapper.camParametersHandler.ManualFNumber);
+        skintone.SetAbstractManualParameter(wrapper.camParametersHandler.Skintone);
+        programshift.SetAbstractManualParameter(wrapper.camParametersHandler.ProgramShift);
+    }
+
+    @Override
+    public void ParametersLoaded() {
+        if (wrapper != null)
+            setWrapper();
+    }
+
+    //######## ManualButton Stuff#####
+    View.OnClickListener manualButtonClickListner = new View.OnClickListener() {
+        @Override
+        public void onClick(View v)
+        {
+            if(v == currentButton && seekbar.getVisibility() == View.VISIBLE)
+                seekbar.setVisibility(View.GONE);
+            else
+            {
+                if (seekbar.getVisibility() == View.GONE)
+                    seekbar.setVisibility(View.VISIBLE);
+                currentButton = (ManualButton) v;
+                seekbar.SetStringValues(currentButton.getStringValues());
+                seekbar.setProgress(currentButton.getCurrentItem());
+            }
+
+        }
+    };
+
+    //#########################SEEKBAR STUFF#############################
+
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser)
+    {
+        currentButton.setValueToParameters(progress);
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
+}
---
+++ themesample/src/main/java/troop/com/themesample/views/ManualButton.java
@@ -0,0 +1,242 @@
+package troop.com.themesample.views;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.os.Handler;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.troop.freedcam.i_camera.parameters.AbstractManualParameter;
+import com.troop.freedcam.sonyapi.parameters.manual.BaseManualParameterSony;
+import com.troop.freedcam.ui.AppSettingsManager;
+
+import troop.com.themesample.R;
+
+/**
+ * Created by troop on 08.12.2015.
+ */
+public class ManualButton extends LinearLayout implements AbstractManualParameter.I_ManualParameterEvent
+{
+
+    String[] parameterValues;
+    AbstractManualParameter parameter;
+    AppSettingsManager appSettingsManager;
+    String settingsname;
+    TextView headerTextView;
+    TextView valueTextView;
+    Handler handler;
+    int realMin;
+    int realMax;
+
+    public ManualButton(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public ManualButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+        TypedArray a = context.getTheme().obtainStyledAttributes(
+                attrs,
+                R.styleable.ManualItem,
+                0, 0
+        );
+        //try to set the attributs
+        try
+        {
+            headerTextView.setText(a.getText(R.styleable.ManualItem_Header));
+
+        }
+        finally {
+            a.recycle();
+        }
+    }
+
+    public ManualButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    private void init(Context context)
+    {
+        handler = new Handler();
+        LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.manualbutton, this);
+        this.headerTextView = (TextView)findViewById(R.id.manualbutton_headertext);
+        headerTextView.setSelected(true);
+        this.valueTextView = (TextView)findViewById(R.id.manualbutton_valuetext);
+        valueTextView.setSelected(true);
+    }
+
+    public void SetAbstractManualParameter(AbstractManualParameter parameter)
+    {
+        this.parameter = parameter;
+        if (parameter != null) {
+            parameter.addEventListner(this);
+            if (parameter.IsSupported())
+            {
+                String txt = parameter.GetStringValue();
+                if (txt != null && !txt.equals(""))
+                    valueTextView.setText(txt);
+                else
+                    valueTextView.setText(parameter.GetValue()+"");
+
+                onIsSupportedChanged(parameter.IsSupported());
+                onIsSetSupportedChanged(parameter.IsSetSupported());
+                realMax = parameter.GetMaxValue();
+                realMin = parameter.GetMinValue();
+                parameterValues = parameter.getStringValues();
+            }
+            else
+                onIsSupportedChanged(false);
+        }
+        else
+            onIsSupportedChanged(false);
+
+    }
+
+    public void SetStuff(AppSettingsManager appSettingsManager, String settingsName)
+    {
+        this.appSettingsManager = appSettingsManager;
+        this.settingsname = settingsName;
+    }
+
+    @Override
+    public void onIsSupportedChanged(final boolean value) {
+        this.post(new Runnable() {
+            @Override
+            public void run() {
+                final String txt = headerTextView.getText().toString();
+                Log.d(txt, "isSupported:" + value);
+                if (value)
+                    ManualButton.this.setVisibility(VISIBLE);
+                else
+                    ManualButton.this.setVisibility(GONE);
+            }
+        });
+    }
+
+    @Override
+    public void onIsSetSupportedChanged(final boolean value) {
+        post(new Runnable() {
+            @Override
+            public void run() {
+                if (value) {
+                    ManualButton.this.setEnabled(true);
+
+                } else {
+                    ManualButton.this.setEnabled(false);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onMaxValueChanged(int max) {
+
+    }
+
+    @Override
+    public void onMinValueChanged(int min) {
+
+    }
+
+    @Override
+    public void onCurrentValueChanged(int current) {
+        setTextValue(current);
+    }
+
+    @Override
+    public void onValuesChanged(String[] values) {
+        this.parameterValues = values;
+    }
+
+    @Override
+    public void onCurrentStringValueChanged(final String value) {
+        valueTextView.post(new Runnable() {
+            @Override
+            public void run() {
+                valueTextView.setText(value);
+            }
+        });
+    }
+
+    private void setTextValue(final int current)
+    {
+        valueTextView.post(new Runnable() {
+            @Override
+            public void run() {
+                String txt = getStringValue(current);
+                if (txt != null && !txt.equals("") && !txt.equals("null"))
+                    valueTextView.setText(txt);
+                else
+                    valueTextView.setText(current+"");
+            }
+        });
+
+    }
+
+    public String getStringValue(int pos)
+    {
+        if(parameterValues == null)
+            parameterValues = parameter.getStringValues();
+        if (parameterValues != null && parameterValues.length > 0)
+        {
+            if (pos > parameterValues.length)
+                return parameterValues[parameterValues.length-1];
+            else if (pos < 0)
+                return parameterValues[0];
+            else
+                return parameterValues[pos];
+        }
+        else if (parameterValues == null)
+            return parameter.GetStringValue();
+
+        return null;
+    }
+
+    public String[] getStringValues()
+    {
+        return parameterValues;
+    }
+
+    public int getCurrentItem(){return  parameter.GetValue();}
+
+    boolean currentlysettingsparameter = false;
+    public void setValueToParameters(final int value)
+    {
+        if (currentlysettingsparameter)
+            return;
+        handler.post(new Runnable() {
+            @Override
+            public void run()
+            {
+                currentlysettingsparameter = true;
+                int runValue = value;
+                if (!(parameter instanceof BaseManualParameterSony) && settingsname != null) {
+                    if (realMin < 0)
+                        appSettingsManager.setString(settingsname, (runValue + realMin) + "");
+                    else
+                        appSettingsManager.setString(settingsname, runValue + "");
+                }
+                if (realMin < 0)
+                    runValue += realMin;
+                if (runValue > realMax) {
+                    Log.e(headerTextView.getText().toString(), "value bigger then max");
+                    return;
+                }
+                if (runValue < realMin)
+                    runValue = realMin;
+                if (runValue > realMax)
+                    runValue = realMax;
+                parameter.SetValue(runValue);
+                currentlysettingsparameter = false;
+            }
+        });
+
+    }
+
+}
