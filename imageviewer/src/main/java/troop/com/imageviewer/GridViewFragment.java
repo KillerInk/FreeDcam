748e09667c441cd24d6b3947f619cea94323aff0
troop <trooopiii@googlemail.com>
2015-12-11 16:43:43 +0100

    cached images for gridview

--- imageviewer/src/main/java/troop/com/imageviewer/GridViewFragment.java
+++ imageviewer/src/main/java/troop/com/imageviewer/GridViewFragment.java
@@ -9,12 +9,15 @@ import android.graphics.drawable.Drawable;
 import android.media.ThumbnailUtils;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.util.LruCache;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -28,6 +31,11 @@ import android.widget.ImageView;
 import com.defcomk.jni.libraw.RawUtils;
 
 import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.lang.ref.WeakReference;
 
 /**
@@ -41,6 +49,16 @@ public class GridViewFragment extends Fragment implements AdapterView.OnItemClic
     File[] files;
     int mImageThumbSize = 0;
 
+    private DiskLruCache mDiskLruCache;
+    private final Object mDiskCacheLock = new Object();
+    private boolean mDiskCacheStarting = true;
+    private static final int DISK_CACHE_SIZE = 1024 * 1024 * 10; // 10MB
+    private static final String DISK_CACHE_SUBDIR = "thumbnails";
+    final String TAG = GridViewFragment.class.getSimpleName();
+
+
+    private LruCache<String, Bitmap> mMemoryCache;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
@@ -48,8 +66,155 @@ public class GridViewFragment extends Fragment implements AdapterView.OnItemClic
         view = inflater.inflate(R.layout.gridviewfragment, container, false);
         this.gridView = (GridView) view.findViewById(R.id.gridView);
         mImageThumbSize = getResources().getDimensionPixelSize(R.dimen.image_thumbnail_size);
+        // Get max available VM memory, exceeding this amount will throw an
+        // OutOfMemory exception. Stored in kilobytes as LruCache takes an
+        // int in its constructor.
+        final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+
+        // Use 1/8th of the available memory for this memory cache.
+        final int cacheSize = maxMemory / 8;
+
+        mMemoryCache = new LruCache<String, Bitmap>(cacheSize) {
+            @Override
+            protected int sizeOf(String key, Bitmap bitmap) {
+                // The cache size will be measured in kilobytes rather than
+                // number of items.
+                return bitmap.getByteCount() / 1024;
+            }
+        };
+
+        File cacheDir = getDiskCacheDir(getActivity(), DISK_CACHE_SUBDIR);
+        new InitDiskCacheTask().execute(cacheDir);
+
+
 
         return view;
+    }
+
+    class InitDiskCacheTask extends AsyncTask<File, Void, Void> {
+        @Override
+        protected Void doInBackground(File... params) {
+            synchronized (mDiskCacheLock) {
+                File cacheDir = params[0];
+                try {
+                    mDiskLruCache = DiskLruCache.open(cacheDir, 1,1, DISK_CACHE_SIZE);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                mDiskCacheStarting = false; // Finished initialization
+                mDiskCacheLock.notifyAll(); // Wake any waiting threads
+            }
+            return null;
+        }
+    }
+
+    public void addBitmapToCache(String data, Bitmap value) {
+        //BEGIN_INCLUDE(add_bitmap_to_cache)
+        if (data == null || value == null) {
+            return;
+        }
+
+        // Add to memory cache
+        if (mMemoryCache != null) {
+
+            mMemoryCache.put(data, value);
+        }
+
+        synchronized (mDiskCacheLock) {
+            // Add to disk cache
+            if (mDiskLruCache != null) {
+
+                OutputStream out = null;
+                try {
+                    DiskLruCache.Snapshot snapshot = mDiskLruCache.get(data);
+                    if (snapshot == null) {
+                        final DiskLruCache.Editor editor = mDiskLruCache.edit(data);
+                        if (editor != null) {
+                            out = editor.newOutputStream(0);
+                            value.compress(
+                                    Bitmap.CompressFormat.JPEG,70, out);
+                            editor.commit();
+                            out.close();
+                        }
+                    } else {
+                        snapshot.getInputStream(0).close();
+                    }
+                } catch (final IOException e) {
+                    Log.e(TAG, "addBitmapToCache - " + e);
+                } catch (Exception e) {
+                    Log.e(TAG, "addBitmapToCache - " + e);
+                } finally {
+                    try {
+                        if (out != null) {
+                            out.close();
+                        }
+                    } catch (IOException e) {}
+                }
+            }
+        }
+        //END_INCLUDE(add_bitmap_to_cache)
+    }
+
+    public Bitmap getBitmapFromDiskCache(String data) {
+        //BEGIN_INCLUDE(get_bitmap_from_disk_cache)
+        //final String key = hashKeyForDisk(data);
+        Bitmap bitmap = null;
+
+        synchronized (mDiskCacheLock) {
+            while (mDiskCacheStarting) {
+                try {
+                    mDiskCacheLock.wait();
+                } catch (InterruptedException e) {}
+            }
+            if (mDiskLruCache != null) {
+                InputStream inputStream = null;
+                try {
+                    final DiskLruCache.Snapshot snapshot = mDiskLruCache.get(data);
+                    if (snapshot != null) {
+
+                        inputStream = snapshot.getInputStream(0);
+                        if (inputStream != null) {
+                            FileDescriptor fd = ((FileInputStream) inputStream).getFD();
+
+                            // Decode bitmap, but we don't want to sample so give
+                            // MAX_VALUE as the target dimensions
+                            bitmap = BitmapFactory.decodeFileDescriptor(fd, null, null);
+                        }
+                    }
+                } catch (final IOException e) {
+
+                } finally {
+                    try {
+                        if (inputStream != null) {
+                            inputStream.close();
+                        }
+                    } catch (IOException e) {}
+                }
+            }
+            return bitmap;
+        }
+        //END_INCLUDE(get_bitmap_from_disk_cache)
+    }
+
+    // Creates a unique subdirectory of the designated app cache directory. Tries to use external
+// but if not mounted, falls back on internal storage.
+    public static File getDiskCacheDir(Context context, String uniqueName) {
+        // Check if media is mounted or storage is built-in, if so, try and use external cache dir
+        // otherwise use internal cache dir
+        final String cachePath = context.getCacheDir().getPath();
+
+        return new File(cachePath + File.separator + uniqueName);
+    }
+
+
+    public void addBitmapToMemoryCache(String key, Bitmap bitmap) {
+        if (getBitmapFromMemCache(key) == null) {
+            mMemoryCache.put(key, bitmap);
+        }
+    }
+
+    public Bitmap getBitmapFromMemCache(String key) {
+        return mMemoryCache.get(key);
     }
 
     @Override
@@ -190,37 +355,52 @@ public class GridViewFragment extends Fragment implements AdapterView.OnItemClic
 
     private Bitmap getBitmap(File file)
     {
-        Bitmap response;
-        if (file.getAbsolutePath().endsWith(".jpg"))
+        Bitmap response = getBitmapFromMemCache(file.getName());
+        if (response == null)
         {
-            BitmapFactory.Options options = new BitmapFactory.Options();
-            options.inSampleSize = 20;
-            response = BitmapFactory.decodeFile(file.getAbsolutePath(), options);
-            response = ThumbnailUtils.extractThumbnail(response, mImageThumbSize, mImageThumbSize);
+            Log.d(TAG,"No image in memory try from disk");
+            response = getBitmapFromDiskCache(file.getName());
         }
-        else if (file.getAbsolutePath().endsWith(".mp4"))
+        if (response == null)
         {
-            response = ThumbnailUtils.createVideoThumbnail(file.getAbsolutePath(), MediaStore.Video.Thumbnails.FULL_SCREEN_KIND);
-            response = ThumbnailUtils.extractThumbnail(response, mImageThumbSize, mImageThumbSize);
-        }
-        else if (file.getAbsolutePath().endsWith(".dng")|| file.getAbsolutePath().endsWith(".raw"))
-        {
-            try {
+            Log.d(TAG,"No image in thumbcache try from disk");
+            if (file.getAbsolutePath().endsWith(".jpg")) {
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 20;
+                response = BitmapFactory.decodeFile(file.getAbsolutePath(), options);
+                response = ThumbnailUtils.extractThumbnail(response, mImageThumbSize, mImageThumbSize);
+                if (response != null) {
+                    addBitmapToMemoryCache(file.getName(), response);
+                    addBitmapToCache(file.getName(), response);
+                }
+
+            } else if (file.getAbsolutePath().endsWith(".mp4")) {
+                response = ThumbnailUtils.createVideoThumbnail(file.getAbsolutePath(), MediaStore.Video.Thumbnails.FULL_SCREEN_KIND);
+                response = ThumbnailUtils.extractThumbnail(response, mImageThumbSize, mImageThumbSize);
+                if (response != null)
+                {
+                    addBitmapToCache(file.getName(),response);
+                    addBitmapToMemoryCache(file.getName(), response);
+                }
+            } else if (file.getAbsolutePath().endsWith(".dng") || file.getAbsolutePath().endsWith(".raw")) {
+                try {
 
 
-                response = RawUtils.UnPackRAW(file.getAbsolutePath());
-                if(response != null)
-                    response.setHasAlpha(true);
-                response = ThumbnailUtils.extractThumbnail(response,mImageThumbSize,mImageThumbSize);
+                    response = RawUtils.UnPackRAW(file.getAbsolutePath());
+                    if (response != null)
+                        response.setHasAlpha(true);
+                    response = ThumbnailUtils.extractThumbnail(response, mImageThumbSize, mImageThumbSize);
+                    if (response != null) {
+                        addBitmapToMemoryCache(file.getName(), response);
+                        addBitmapToCache(file.getName(), response);
+                    }
+                } catch (IllegalArgumentException ex) {
+                    response = null;
+
+                }
             }
-            catch (IllegalArgumentException ex)
-            {
-                response = null;
-
-            }
         }
-        else
-            response = null;
+        
 
         return response;
     }
