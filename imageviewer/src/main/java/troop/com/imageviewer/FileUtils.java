c5ee91046ab52d07c4d7664b9a612fde679d9a55
troop <trooopiii@googlemail.com>
2015-12-16 14:32:10 +0100

    load files depending on filetype

--- imageviewer/src/main/java/troop/com/imageviewer/FileUtils.java
+++ imageviewer/src/main/java/troop/com/imageviewer/FileUtils.java
@@ -8,17 +8,32 @@ import java.util.List;
  */
 public class FileUtils
 {
-    public static void readFilesFromFolder(File folder, List<File> fileList)
+    public static void readFilesFromFolder(File folder, List<FileHolder> list, GridViewFragment.FormatTypes formatsToShow)
     {
         File[] folderfiles = folder.listFiles();
         for (File f : folderfiles)
         {
-            if (!f.isDirectory() &&
-                    (f.getAbsolutePath().endsWith(".jpg") ||
-                            f.getAbsolutePath().endsWith(".mp4")||
-                            f.getAbsolutePath().endsWith(".dng")||
-                            f.getAbsolutePath().endsWith(".raw")))
-                fileList.add(f);
+            if (!f.isHidden())
+            {
+                if (formatsToShow == GridViewFragment.FormatTypes.all && (
+                        f.getAbsolutePath().endsWith("jpg")
+                                || f.getAbsolutePath().endsWith("jps")
+                                ||f.getAbsolutePath().endsWith("raw")
+                                ||f.getAbsolutePath().endsWith("dng")
+                                ||   f.getAbsolutePath().endsWith("mp4")
+                ))
+                    list.add(new FileHolder(f));
+                else if(formatsToShow == GridViewFragment.FormatTypes.dng && f.getAbsolutePath().endsWith("dng"))
+                    list.add(new FileHolder(f));
+                else if(formatsToShow == GridViewFragment.FormatTypes.raw && f.getAbsolutePath().endsWith("raw"))
+                    list.add(new FileHolder(f));
+                else if(formatsToShow == GridViewFragment.FormatTypes.jps && f.getAbsolutePath().endsWith("jps"))
+                    list.add(new FileHolder(f));
+                else if(formatsToShow == GridViewFragment.FormatTypes.jpg && f.getAbsolutePath().endsWith("jpg"))
+                    list.add(new FileHolder(f));
+                else if(formatsToShow == GridViewFragment.FormatTypes.mp4 && f.getAbsolutePath().endsWith("mp4"))
+                    list.add(new FileHolder(f));
+            }
         }
     }
 
