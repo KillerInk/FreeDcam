23513539004388f0cefd2e100f57542d99bd564e
Ar4eR07 <molemax@mail.ru>
2016-01-19 02:23:49 +0400

    Swipe gesture rework

--- abstractcamera/src/main/java/com/troop/freedcam/ui/TouchHandler.java
+++ abstractcamera/src/main/java/com/troop/freedcam/ui/TouchHandler.java
@@ -8,85 +8,57 @@ import android.view.MotionEvent;
  */
 public class TouchHandler
 {
-    final int distance = 150;
+    static final int distance = 300;
+    static final int MAX_DURATION = 600;
     public int startX;
     public int startY;
     public int currentX;
     public int currentY;
-    boolean swipe = false;
-    long start;
-    long duration;
-    static final int MAX_DURATION = 3500;
-
+    public int x;
+    public int y;
 
     public boolean onTouchEvent(MotionEvent event)
     {
         boolean fireagain = true;
-
         switch (event.getAction())
         {
             case MotionEvent.ACTION_DOWN:
                 startX = (int) event.getX();
                 startY = (int) event.getY();
-                currentX = (int) event.getX();
-                currentY = (int) event.getY();
-                start = System.currentTimeMillis();
-                Log.d("TouchHAndler", "currentX:" + currentX + " X:" + startX);
                 break;
-           // case MotionEvent.A
             case MotionEvent.ACTION_MOVE:
-                if (startX == 0 && startY == 0)
-                {
-                    startX = (int) event.getX();
-                    startY = (int) event.getY();
-                }
-                currentX = (int) event.getX();
-                currentY = (int) event.getY();
-                detectSwipeDirection();
                 break;
             case MotionEvent.ACTION_UP:
-                long time = System.currentTimeMillis() - start;
-                duration = duration+time;
-                if (duration >= MAX_DURATION)
-                    System.out.println("Long Press Time: "+ duration);
-                if (swipe == false)
+                currentX = (int) event.getX();
+                currentY = (int) event.getY();
+                x = getDistance(startX, currentX);
+                y = getDistance(startY, currentY);
+                long duration = event.getEventTime() - event.getDownTime();
+                if (x >= distance && duration <= MAX_DURATION || y >= distance && duration <= MAX_DURATION)
+                {
+                    Log.d("TouchHAndler", "currentX:" + currentX + " X:" + startX);
+                    if (x >= y) {
+                        if (currentX > startX)
+                            doLeftToRightSwipe();
+                        else
+                            doRightToLeftSwipe();
+                    }
+                    else{
+                        if (currentY > startY)
+                            doTopToBottomSwipe();
+                        else
+                            doBottomToTopSwipe();
+                    }
+                }
+                else if (x < distance && y < distance)
                 {
                     OnClick((int)event.getX(), (int)event.getY());
-
                 }
-                swipe = false;
-                fireagain = false;
-                currentX = 0;
-                startX = 0;
                 break;
         }
-
-
         return fireagain;
     }
 
-    private void detectSwipeDirection()
-    {
-        int x = getDistance(startX, currentX);
-        int y = getDistance(startY, currentY);
-        if (x >= distance || y >= distance)
-        {
-            swipe = true;
-            Log.d("TouchHAndler", "currentX:" + currentX + " X:" + startX);
-            if (x >= y) {
-                if (currentX > startX)
-                    doLeftToRightSwipe();
-                else
-                    doRightToLeftSwipe();
-            }
-            else{
-                if (currentY > startY)
-                    doTopToBottomSwipe();
-                else
-                    doBottomToTopSwipe();
-            }
-        }
-    }
 
     protected void doLeftToRightSwipe()
     {
@@ -106,7 +78,6 @@ public class TouchHandler
 
     protected void OnClick(int x, int y)
     {
-
     }
 
     public static int getDistance(int startvalue, int currentvalue)
@@ -122,6 +93,4 @@ public class TouchHandler
         int dis = startvalue - currentvalue;
         return dis;
     }
-
-
 }
