a82b00598aaa8c009d7e57819c021025bbbdf08f
defcomk <defcomk@gmail.com>
2016-01-23 00:00:36 +0200

    LibTiff Alternative
    DNG Writer

---
+++ androiddng/src/main/jni/libDngWritter/DngWriter.cpp
@@ -0,0 +1,5 @@
+//
+// Created by GeorgeKiarie on 1/22/2016.
+//
+
+#include "DngWriter.h"
---
+++ androiddng/src/main/jni/libDngWritter/DngWriter.h
@@ -0,0 +1,14 @@
+//
+// Created by GeorgeKiarie on 1/22/2016.
+//
+
+#ifndef 3_3_115_DNGWRITER_H
+#define 3_3_115_DNGWRITER_H
+
+
+class DngWriter {
+
+};
+
+
+#endif //3_3_115_DNGWRITER_H
