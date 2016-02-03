fb1e25f028a354863f08d151df348c4dece1a31a
troop <trooopiii@googlemail.com>
2016-02-03 14:55:36 +0100

    VideoModuleG3 fixup

--- camera1/src/main/java/com/troop/freedcam/camera/modules/VideoModuleG3.java
+++ camera1/src/main/java/com/troop/freedcam/camera/modules/VideoModuleG3.java
@@ -62,9 +62,9 @@ public class VideoModuleG3 extends AbstractVideoModule
             Log.e(TAG, "Index :" + frame);
 
             camParametersHandler.FPSRangeLock(frame,frame);
-            recorder.setVideoFrameRate(frame);
+            //recorder.setVideoFrameRate(frame);
 
-            //recorder.setCaptureRate(frame);
+            recorder.setCaptureRate(frame);
 
 
 
@@ -75,11 +75,7 @@ public class VideoModuleG3 extends AbstractVideoModule
         }
         recorder.setVideoSize(prof.videoFrameWidth, prof.videoFrameHeight);
 
-
-        if(!mBitare.equals("200Mbps") || !mBitare.equals("150Mbps") || !mBitare.equals("100Mbps")
-                || !mBitare.equals("80Mbps")|| !mBitare.equals("60Mbps")|| !mBitare.equals("50Mbps")
-                || !mBitare.equals("40Mbps")|| !mBitare.equals("30Mbps")|| !mBitare.equals("10Mbps")
-                || !mBitare.equals("5Mbps")|| !mBitare.equals("5Mbps") ) {
+        if (mBitare.equals("Default") || mBitare.equals("")) {
             recorder.setVideoEncodingBitRate(prof.videoBitRate);
         }
         else {
