9661c3b0b350acd9e69043b5b1393811378664a2
troop <trooopiii@googlemail.com>
2016-04-02 07:03:12 +0200

    catch nullpointer when CACHE is released and bitmap should get added

--- imageviewer/src/main/java/troop/com/imageviewer/BitmapHelper.java
+++ imageviewer/src/main/java/troop/com/imageviewer/BitmapHelper.java
@@ -48,56 +48,61 @@ public class BitmapHelper
 
     public static Bitmap getBitmap(final File file,final boolean thumb,final int mImageThumbSizeW,final int  mImageThumbSizeH)
     {
-        if (CACHE == null)
-            return null;
         Bitmap response = null;
-        if (thumb)
-        {
-            response = CACHE.getBitmapFromMemCache(file.getName() + "_thumb");
-            if (response == null) {
-                //Logger.d(TAG,"No image in memory try from disk");
-                response = CACHE.getBitmapFromDiskCache(file.getName() + "_thumb");
-            }
-        }
-        else
-        {
-            response = CACHE.getBitmapFromMemCache(file.getName());
-            if (response == null) {
-                //Logger.d(TAG,"No image in memory try from disk");
-                response = CACHE.getBitmapFromDiskCache(file.getName());
-            }
-        }
-        if (response == null && file.exists())
-        {
-            if (file.getAbsolutePath().endsWith(StringUtils.FileEnding.JPG) || file.getAbsolutePath().endsWith(StringUtils.FileEnding.JPS))
+        try {
+            if (CACHE == null)
+                return null;
+            response = null;
+            if (thumb)
             {
-                BitmapFactory.Options options = new BitmapFactory.Options();
-                options.inSampleSize = 2;
-                response = BitmapFactory.decodeFile(file.getAbsolutePath(), options);
-            }
-            else if (file.getAbsolutePath().endsWith(StringUtils.FileEnding.MP4))
-                response = ThumbnailUtils.createVideoThumbnail(file.getAbsolutePath(), MediaStore.Video.Thumbnails.FULL_SCREEN_KIND);
-            else if (file.getAbsolutePath().endsWith(StringUtils.FileEnding.DNG)
-                    || file.getAbsolutePath().endsWith(StringUtils.FileEnding.RAW) || file.getAbsolutePath().endsWith(StringUtils.FileEnding.BAYER))
-            {
-                try {
-                    response = RawUtils.UnPackRAW(file.getAbsolutePath());
+                response = CACHE.getBitmapFromMemCache(file.getName() + "_thumb");
+                if (response == null) {
+                    //Logger.d(TAG,"No image in memory try from disk");
+                    response = CACHE.getBitmapFromDiskCache(file.getName() + "_thumb");
                 }
-                catch (IllegalArgumentException ex)
+            }
+            else
+            {
+                response = CACHE.getBitmapFromMemCache(file.getName());
+                if (response == null) {
+                    //Logger.d(TAG,"No image in memory try from disk");
+                    response = CACHE.getBitmapFromDiskCache(file.getName());
+                }
+            }
+            if (response == null && file.exists())
+            {
+                if (file.getAbsolutePath().endsWith(StringUtils.FileEnding.JPG) || file.getAbsolutePath().endsWith(StringUtils.FileEnding.JPS))
                 {
+                    BitmapFactory.Options options = new BitmapFactory.Options();
+                    options.inSampleSize = 2;
+                    response = BitmapFactory.decodeFile(file.getAbsolutePath(), options);
+                }
+                else if (file.getAbsolutePath().endsWith(StringUtils.FileEnding.MP4))
+                    response = ThumbnailUtils.createVideoThumbnail(file.getAbsolutePath(), MediaStore.Video.Thumbnails.FULL_SCREEN_KIND);
+                else if (file.getAbsolutePath().endsWith(StringUtils.FileEnding.DNG)
+                        || file.getAbsolutePath().endsWith(StringUtils.FileEnding.RAW) || file.getAbsolutePath().endsWith(StringUtils.FileEnding.BAYER))
+                {
+                    try {
+                        response = RawUtils.UnPackRAW(file.getAbsolutePath());
+                    }
+                    catch (IllegalArgumentException ex)
+                    {
 
+                    }
+                }
+                if (response != null && CACHE != null)
+                {
+                    CACHE.addBitmapToCache(file.getName(), response);
+                    if (thumb)
+                    {
+                        response = ThumbnailUtils.extractThumbnail(response, mImageThumbSizeW, mImageThumbSizeH);
+                        CACHE.addBitmapToCache(file.getName() + "_thumb", response);
+                    } else
+                        CACHE.addBitmapToCache(file.getName() + "_thumb", ThumbnailUtils.extractThumbnail(response, mImageThumbSizeW, mImageThumbSizeH));
                 }
             }
-            if (response != null && CACHE != null)
-            {
-                CACHE.addBitmapToCache(file.getName(), response);
-                if (thumb)
-                {
-                    response = ThumbnailUtils.extractThumbnail(response, mImageThumbSizeW, mImageThumbSizeH);
-                    CACHE.addBitmapToCache(file.getName() + "_thumb", response);
-                } else
-                    CACHE.addBitmapToCache(file.getName() + "_thumb", ThumbnailUtils.extractThumbnail(response, mImageThumbSizeW, mImageThumbSizeH));
-            }
+        } catch (NullPointerException e) {
+            e.printStackTrace();
         }
         return  response;
     }
