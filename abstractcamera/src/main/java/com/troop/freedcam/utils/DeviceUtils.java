f7e83b6a163d13b0182c8eb73de6426135295deb
defcomk <defcomk@gmail.com>
2016-04-17 23:29:18 +0200

    Vodafone/Alcatel 985N
    (cherry picked from commit bd538cad4fb679079fb52f468b0b1ae4dc9e35d5)

--- abstractcamera/src/main/java/com/troop/freedcam/utils/DeviceUtils.java
+++ abstractcamera/src/main/java/com/troop/freedcam/utils/DeviceUtils.java
@@ -175,6 +175,8 @@ public class DeviceUtils
             return Devices.p8;
         else if(isHuawei_Honor_6())
             return Devices.honor6;
+        else if(is985N())
+            return Devices.Alcatel_985n;
         else
             return Devices.UNKNOWN;
 
@@ -183,6 +185,7 @@ public class DeviceUtils
     public enum Devices
     {
         UNKNOWN,
+        Alcatel_985n,
         p8,
         p8lite,
         honor6,
@@ -293,7 +296,8 @@ public class DeviceUtils
             Devices.SonyM4_QC,
             Devices.HTC_OneA9,
             Devices.Lenovo_K50_MTK,
-            Devices.ForwardArt_MTK
+            Devices.ForwardArt_MTK,
+            Devices.Alcatel_985n
     };
 
     public static Devices[] camera1NO_RAW_STREAM = {
@@ -504,6 +508,12 @@ public class DeviceUtils
     {
         return isDevice(contex.getResources().getStringArray(R.array.meizu_mx4));
     }
+
+    private static boolean is985N()
+    {
+        return isDevice(contex.getResources().getStringArray(R.array.Alcatel_985N));
+    }
+
     private static boolean isMeizuMX5()
     {
         return isDevice(contex.getResources().getStringArray(R.array.meizu_mx5));
