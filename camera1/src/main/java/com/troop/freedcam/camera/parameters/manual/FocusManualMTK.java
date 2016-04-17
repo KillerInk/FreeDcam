f817d7da105cf509f10fd97e2c1e75c0e568fdf7
defcomk <defcomk@gmail.com>
2016-04-17 23:29:54 +0200

    Working Mediatek Manual Focus
    (cherry picked from commit 2abca9974350ec55944869445af162542c5721c6)

--- camera1/src/main/java/com/troop/freedcam/camera/parameters/manual/FocusManualMTK.java
+++ camera1/src/main/java/com/troop/freedcam/camera/parameters/manual/FocusManualMTK.java
@@ -11,10 +11,17 @@ import java.util.HashMap;
 public class FocusManualMTK extends BaseFocusManual {
     public FocusManualMTK(HashMap<String, String> parameters, String value, String maxValue, String MinValue, String manualFocusModeString, AbstractParameterHandler camParametersHandler, float step, int manualFocusType) {
         super(parameters, value, maxValue, MinValue, manualFocusModeString, camParametersHandler, step, manualFocusType);
+        isSupported = true;
+        isVisible = isSupported;
     }
 
     public FocusManualMTK(HashMap<String, String> parameters, String value, int min, int max, String manualFocusModeString, AbstractParameterHandler camParametersHandler, float step, int manualFocusType) {
         super(parameters, value, min, max, manualFocusModeString, camParametersHandler, step, manualFocusType);
+        this.isSupported = true;
+        this.isVisible = true;
+        this.manualFocusModeString = manualFocusModeString;
+        this.stringvalues = createStringArray(min,max,step);
+        //this.manualFocusType = manualFocusType;
     }
 
     @Override
