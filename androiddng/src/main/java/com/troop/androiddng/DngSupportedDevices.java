0cf2723bd0dfa13943ecd280f6763621d7068b35
troop <trooopiii@googlemail.com>
2016-04-29 11:08:43 +0200

    use correct filesize for jiayu s3

--- androiddng/src/main/java/com/troop/androiddng/DngSupportedDevices.java
+++ androiddng/src/main/java/com/troop/androiddng/DngSupportedDevices.java
@@ -68,19 +68,23 @@ public class DngSupportedDevices
                         Matrixes.imx230_reduction_matrix2,
                         Matrixes.imx230_3x1_matrix);
             case 26257920://SOny C5 probable imx 214 rggb
-                return new DngProfile(64, 4208, 3120, Plain, RGGB, 0, Matrixes.imx214_identity_matrix1, Matrixes.imx214_identity_matrix2, Matrixes.Nexus6_identity_neutra,
-                        Matrixes.imx214_foward_matrix1,
-                        Matrixes.imx214_foward_matrix2,
-                        Matrixes.G4_reduction_matrix1,
-                        Matrixes.G4_reduction_matrix2,
-                        Matrixes.G4_noise_3x1_matrix);
-            case 25958400://Jiayu_S3
-                return new DngProfile(64, 4160, 3120, Plain, BGGR, 0, Matrixes.imx214_identity_matrix1, Matrixes.imx214_identity_matrix2, Matrixes.Nexus6_identity_neutra,
-                        Matrixes.imx214_foward_matrix1,
-                        Matrixes.imx214_foward_matrix2,
-                        Matrixes.G4_reduction_matrix1,
-                        Matrixes.G4_reduction_matrix2,
-                        Matrixes.G4_noise_3x1_matrix);
+                switch (device)
+                {
+                    case SonyC5_MTK:
+                        return new DngProfile(64, 4208, 3120, Plain, RGGB, 0, Matrixes.imx214_identity_matrix1, Matrixes.imx214_identity_matrix2, Matrixes.Nexus6_identity_neutra,
+                                Matrixes.imx214_foward_matrix1,
+                                Matrixes.imx214_foward_matrix2,
+                                Matrixes.G4_reduction_matrix1,
+                                Matrixes.G4_reduction_matrix2,
+                                Matrixes.G4_noise_3x1_matrix);
+                    case Jiayu_S3:
+                        return new DngProfile(64, 4208, 3120, Plain, BGGR, 0, Matrixes.imx214_identity_matrix1, Matrixes.imx214_identity_matrix2, Matrixes.Nexus6_identity_neutra,
+                                Matrixes.imx214_foward_matrix1,
+                                Matrixes.imx214_foward_matrix2,
+                                Matrixes.G4_reduction_matrix1,
+                                Matrixes.G4_reduction_matrix2,
+                                Matrixes.G4_noise_3x1_matrix);
+                }
             case 26357760: //oneplus
                 return new DngProfile(16,4224 ,3120 ,Plain, BGGR, 0,
                         Matrixes.Nexus6_identity_matrix1,
