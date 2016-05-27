dd902a3fcab8f0a43c2b34a01838ff25037ecbb9
troop <trooopiii@googlemail.com>
2016-05-27 10:11:30 +0200

    rs to sdk 18, thats the sdk we support i

--- app/src/main/rs/focus_peak_cam1.rs
+++ app/src/main/rs/focus_peak_cam1.rs
@@ -61,8 +61,11 @@
         {
             rgb.r =0; rgb.g = 0;  rgb.b = 255;
         }
+        if (rgb.r > 255) rgb.r = 255; if(rgb.r < 0) rgb.r = 0;
+        if (rgb.g > 255) rgb.g = 255; if(rgb.g < 0) rgb.g = 0;
+        if (rgb.b > 255) rgb.b = 255; if(rgb.b < 0) rgb.b = 0;
 
-        uchar4 out = convert_uchar4(clamp(rgb, 0, 255));
+        uchar4 out = convert_uchar4(rgb);
 
         return out;
     }
