61fa6451c4fe29ad539a75d624edd39fc95bf7e8
troop <trooopiii@googlemail.com>
2016-06-02 18:48:28 +0200

    move I_Mobile_IStyleQ6 and Jiayu_S3 to mtk

--- app/src/main/java/com/freedcam/apis/camera1/camera/parameters/device/mtk/Jiayu_S3.java
+++ app/src/main/java/com/freedcam/apis/camera1/camera/parameters/device/mtk/Jiayu_S3.java
@@ -1,4 +1,4 @@
-package com.freedcam.apis.camera1.camera.parameters.device.qcom;
+package com.freedcam.apis.camera1.camera.parameters.device.mtk;
 
 import android.hardware.Camera;
 import android.os.Handler;
@@ -8,24 +8,25 @@ import com.freedcam.apis.basecamera.camera.parameters.modes.MatrixChooserParamet
 import com.freedcam.apis.camera1.camera.CameraHolderApi1;
 import com.freedcam.apis.camera1.camera.CameraUiWrapper;
 import com.freedcam.apis.camera1.camera.parameters.CamParametersHandler;
+import com.freedcam.apis.camera1.camera.parameters.device.BaseMTKDevice;
 import com.freedcam.apis.camera1.camera.parameters.device.BaseQcomDevice;
 import com.troop.androiddng.DngProfile;
 
 /**
  * Created by troop on 01.06.2016.
  */
-public class I_Mobile_IStyleQ6 extends BaseQcomDevice {
-    public I_Mobile_IStyleQ6(Handler uihandler, Camera.Parameters parameters, CameraUiWrapper cameraUiWrapper) {
+public class Jiayu_S3 extends BaseMTKDevice {
+    public Jiayu_S3(Handler uihandler, Camera.Parameters parameters, CameraUiWrapper cameraUiWrapper) {
         super(uihandler, parameters, cameraUiWrapper);
     }
 
     @Override
     public DngProfile getDngProfile(int filesize)
     {
-        switch (filesize) {
-            case 9631728: //I_Mobile_I_StyleQ6
-                return new DngProfile(0, 2532, 1902, DngProfile.Plain, DngProfile.GRBG, 0,
-                        matrixChooserParameter.GetCustomMatrix(MatrixChooserParameter.OmniVision));
+        switch (filesize)
+        {
+            case 26257920:
+                return new DngProfile(64, 4208, 3120, DngProfile.Plain, DngProfile.RGGB, 0, matrixChooserParameter.GetCustomMatrix(MatrixChooserParameter.IMX214));
         }
         return null;
     }
