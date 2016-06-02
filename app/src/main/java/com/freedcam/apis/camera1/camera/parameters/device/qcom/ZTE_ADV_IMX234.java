9a12945441a766d0b138fbe00c3a1bf9fdbb5637
defcomk <defcomk@gmail.com>
2016-06-02 21:47:11 +0200

    ZTE Devices Info Fill

--- app/src/main/java/com/freedcam/apis/camera1/camera/parameters/device/qcom/ZTE_ADV_IMX234.java
+++ app/src/main/java/com/freedcam/apis/camera1/camera/parameters/device/qcom/ZTE_ADV_IMX234.java
@@ -3,7 +3,13 @@ package com.freedcam.apis.camera1.camera.parameters.device.qcom;
 import android.hardware.Camera;
 import android.os.Handler;
 
+import com.freedcam.apis.basecamera.camera.parameters.manual.AbstractManualParameter;
+import com.freedcam.apis.basecamera.camera.parameters.modes.MatrixChooserParameter;
 import com.freedcam.apis.camera1.camera.CameraUiWrapper;
+import com.freedcam.apis.camera1.camera.parameters.KEYS;
+import com.freedcam.apis.camera1.camera.parameters.manual.BaseCCTManual;
+import com.freedcam.apis.camera1.camera.parameters.manual.BaseFocusManual;
+import com.freedcam.apis.camera1.camera.parameters.manual.ShutterManualZTE;
 import com.troop.androiddng.DngProfile;
 
 /**
@@ -15,7 +21,29 @@ public class ZTE_ADV_IMX234 extends ZTE_ADV {
     }
 
     @Override
+    public AbstractManualParameter getExposureTimeParameter() {
+        return new ShutterManualZTE(parameters, cameraHolder, camParametersHandler);
+    }
+
+    @Override
+    public AbstractManualParameter getManualFocusParameter() {
+        return new BaseFocusManual(parameters, KEYS.KEY_MANUAL_FOCUS_POSITION,0,79,KEYS.KEY_FOCUS_MODE_MANUAL,camParametersHandler,1,1);
+    }
+
+    @Override
+    public AbstractManualParameter getCCTParameter() {
+        return new BaseCCTManual(parameters,KEYS.WB_MANUAL,8000,2000,camParametersHandler,100, KEYS.WB_MODE_MANUAL_CCT);
+    }
+
+    @Override
     public DngProfile getDngProfile(int filesize) {
+        switch (filesize)
+        {
+            case 17522688:
+                return new DngProfile(64, 4212, 3120, DngProfile.Qcom, DngProfile.RGGB, DngProfile.ROWSIZE, matrixChooserParameter.GetCustomMatrix(MatrixChooserParameter.NEXUS6));
+            case 16424960:
+                return new DngProfile(64, 4208, 3120, DngProfile.Mipi, DngProfile.RGGB, DngProfile.ROWSIZE,matrixChooserParameter.GetCustomMatrix(MatrixChooserParameter.NEXUS6));
+        }
         return null;
     }
 }
--- app/src/main/java/com/freedcam/apis/camera1/camera/parameters/manual/ShutterManualZTE.java
+++ app/src/main/java/com/freedcam/apis/camera1/camera/parameters/manual/ShutterManualZTE.java
@@ -16,7 +16,7 @@ public class ShutterManualZTE extends BaseManualParameter
     private I_CameraHolder baseCameraHolder;
     private final String TAG = ShutterManualZTE.class.getSimpleName();
 
-    final String Z5SShutterValues = "Auto,1/90000,1/75000,1/50000,1/45000,1/30000,1/20000,1/12000,1/10000"+
+    private final String Z5SShutterValues = "Auto,1/90000,1/75000,1/50000,1/45000,1/30000,1/20000,1/12000,1/10000"+
             ",1/8000,1/6400,1/5000,1/4000,1/3200,1/2500,1/2000,1/1600,1/1250,1/1000"+
             ",1/800,1/700,1/600,1/500,1/400,1/300,1/200,1/125,1/100,1/85,1/75,1/65"+
             ",1/55,1/45,1/35,1/25,1/20,1/15,1/13,1/10,1/9,1/8,1/7,1/6,1/5,1/4,1/3,1/2"+
@@ -40,7 +40,10 @@ public class ShutterManualZTE extends BaseManualParameter
     public ShutterManualZTE(Camera.Parameters parameters, I_CameraHolder baseCameraHolder, CamParametersHandler camParametersHandler) {
         super(parameters, "", "", "", camParametersHandler,1);
         this.baseCameraHolder = baseCameraHolder;
-        stringvalues = Z7ShutterValues.split(",");
+        if(DeviceUtils.IS(DeviceUtils.Devices.ZTE_ADV))
+            stringvalues = Z5SShutterValues.split(",");
+        else
+            stringvalues = Z7ShutterValues.split(",");
 
         this.isSupported = true;
     }
