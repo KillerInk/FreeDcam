ce8168d8fe4c17c5be6e59a0d8108d99c8130dbd
troop <trooopiii@googlemail.com>
2016-06-05 16:23:40 +0200

    ShutterManualMeizu add Auto
    (cherry picked from commit 95aad4d6222538cb1a272dad329ea5bbcd9f6fd8)

--- app/src/main/java/com/freedcam/apis/camera1/camera/parameters/manual/ShutterManualMeizu.java
+++ app/src/main/java/com/freedcam/apis/camera1/camera/parameters/manual/ShutterManualMeizu.java
@@ -15,7 +15,7 @@ public class ShutterManualMeizu extends BaseManualParameter
     private I_CameraHolder baseCameraHolder;
     private final String TAG = ShutterManualMeizu.class.getSimpleName();
 
-    private final String MX4Shutter ="1/8000,1/6400,1/5000,1/4000,1/3200,1/2500,1/2000,1/1600,1/1250,1/1000"+
+    private final String MX4Shutter ="Auto,1/8000,1/6400,1/5000,1/4000,1/3200,1/2500,1/2000,1/1600,1/1250,1/1000"+
             ",1/800,1/700,1/600,1/500,1/400,1/300,1/200,1/125,1/100,1/85,1/75,1/65"+
             ",1/55,1/45,1/35,1/25,1/20,1/15,1/13,1/10,1/9,1/8,1/7,1/6,1/5";
 
