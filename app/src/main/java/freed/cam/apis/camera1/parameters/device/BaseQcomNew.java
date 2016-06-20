759f76954634734f87130f195f9fe86c66cfe0d0
troop <trooopiii@googlemail.com>
2016-06-20 14:15:31 +0200

    let BaseQcomNew extend BaseQcomDevice

--- app/src/main/java/freed/cam/apis/camera1/parameters/device/BaseQcomNew.java
+++ app/src/main/java/freed/cam/apis/camera1/parameters/device/BaseQcomNew.java
@@ -30,14 +30,14 @@ import freed.cam.apis.camera1.parameters.ParametersHandler;
 import freed.cam.apis.camera1.parameters.manual.focus.BaseFocusManual;
 import freed.cam.apis.camera1.parameters.manual.qcom.BaseISOManual;
 import freed.cam.apis.camera1.parameters.manual.qcom.ShutterManual_ExposureTime_Micro;
-import freed.cam.apis.camera1.parameters.manual.whitebalance.BaseWB_CCT_QC;
+import freed.cam.apis.camera1.parameters.manual.whitebalance.BaseCCTManual;
 import freed.cam.apis.camera1.parameters.modes.BaseModeParameter;
 import freed.dng.DngProfile;
 
 /**
  * Created by troop on 02.06.2016.
  */
-public class BaseQcomNew extends AbstractDevice
+public class BaseQcomNew extends BaseQcomDevice
 {
     public BaseQcomNew(Parameters parameters, CameraWrapperInterface cameraUiWrapper) {
         super(parameters, cameraUiWrapper);
@@ -65,10 +65,6 @@ public class BaseQcomNew extends AbstractDevice
         return new BaseFocusManual(parameters, KEYS.KEY_MANUAL_FOCUS_POSITION,0, 100,KEYS.KEY_FOCUS_MODE_MANUAL, cameraUiWrapper,1,2);
     }
 
-    @Override
-    public ManualParameterInterface getCCTParameter() {
-        return new BaseWB_CCT_QC(parameters, 8000,2000, cameraUiWrapper,100, KEYS.WB_MODE_MANUAL_CCT);
-    }
 
     @Override
     public ManualParameterInterface getSkintoneParameter() {
