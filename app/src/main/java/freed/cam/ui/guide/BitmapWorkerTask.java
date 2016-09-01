20b732b5ce7ef698e67e3579424e9bc69f020818
troop <trooopiii@googlemail.com>
2016-09-01 15:39:52 +0200

    load Guides in background

---
+++ app/src/main/java/freed/cam/ui/guide/BitmapWorkerTask.java
@@ -0,0 +1,104 @@
+/*
+ *
+ *     Copyright (C) 2015 Ingo Fuchs
+ *     This program is free software; you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation; either version 2 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc.,
+ *     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * /
+ */
+
+package freed.cam.ui.guide;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.widget.ImageView;
+
+import java.lang.ref.WeakReference;
+
+/**
+ * Created by troop on 01.09.2016.
+ */
+public class BitmapWorkerTask extends AsyncTask<Integer, Void, Bitmap> {
+    private final WeakReference<ImageView> imageViewReference;
+    private int data = 0;
+    private int w;
+    private int h;
+    private Resources resources;
+
+    public BitmapWorkerTask(ImageView imageView, Resources resources) {
+        // Use a WeakReference to ensure the ImageView can be garbage collected
+        imageViewReference = new WeakReference<ImageView>(imageView);
+        w = imageView.getWidth();
+        h = imageView.getHeight();
+        this.resources = resources;
+    }
+
+    // Decode image in background.
+    @Override
+    protected Bitmap doInBackground(Integer... params) {
+        data = params[0];
+        return decodeSampledBitmapFromResource(resources,data, w, h);
+    }
+
+    // Once complete, see if ImageView is still around and set bitmap.
+    @Override
+    protected void onPostExecute(Bitmap bitmap) {
+        if (imageViewReference != null && bitmap != null) {
+            final ImageView imageView = imageViewReference.get();
+            if (imageView != null) {
+                imageView.setImageBitmap(bitmap);
+            }
+        }
+    }
+
+    private Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
+                                                         int reqWidth, int reqHeight) {
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res, resId, options);
+    }
+
+    private int calculateInSampleSize(
+            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) >= reqHeight
+                    && (halfWidth / inSampleSize) >= reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
+    }
+}
--- app/src/main/java/freed/cam/ui/guide/GuideHandler.java
+++ app/src/main/java/freed/cam/ui/guide/GuideHandler.java
@@ -1,5 +1,9 @@
 package freed.cam.ui.guide;
 
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,6 +14,8 @@ import android.widget.ImageView;
 import com.troop.freedcam.R.drawable;
 import com.troop.freedcam.R.id;
 import com.troop.freedcam.R.layout;
+
+import java.lang.ref.WeakReference;
 
 import freed.cam.apis.basecamera.CameraWrapperInterface;
 import freed.cam.apis.basecamera.parameters.I_ParametersLoaded;
@@ -64,127 +70,123 @@ public class GuideHandler extends Fragment implements I_ModeParameterEvent , I_P
 
     private void SetViewG(final String str)
     {
+        BitmapWorkerTask task = new BitmapWorkerTask(img, getResources());
+        if (quckRationMath < 1.44f) {
+            switch (str) {
+                case "Golden Spiral":
+                    task.execute(drawable.ic_guide_golden_spiral_4_3);
+                    break;
+                case "Rule Of Thirds":
+                    task.execute(drawable.ic_guide_rule_3rd_4_3);
+                    break;
+                case "Square 1:1":
+                    task.execute(drawable.ic_guide_insta_1_1);
+                    break;
+                case "Square 4:3":
+                    task.execute(drawable.ic_guide_insta_4_3);
+                    break;
+                case "Square 16:9":
+                    task.execute(drawable.ic_guide_insta_16_9);
+                    break;
+                case "Diagonal Type 1":
+                    task.execute(drawable.ic_guide_diagonal_type_1_4_3);
+                    break;
+                case "Diagonal Type 2":
+                    task.execute(drawable.ic_guide_diagonal_type_2_4_3);
+                    break;
+                case "Diagonal Type 3":
+                    task.execute(drawable.ic_guide_diagonal_type_3);
+                    break;
+                case "Diagonal Type 4":
+                    task.execute(drawable.ic_guide_diagonal_type_4);
+                    break;
+                case "Diagonal Type 5":
+                    task.execute(drawable.ic_guide_diagonal_type_5);
+                    break;
+                case "Golden Ratio":
+                    task.execute(drawable.ic_guide_golden_ratio_type_1_4_3);
+                    break;
+                case "Golden Hybrid":
+                    task.execute(drawable.ic_guide_golden_spriral_ratio_4_3);
+                    break;
+                case "Golden R/S 1":
+                    task.execute(drawable.ic_guide_golden_fuse1_4_3);
+                    break;
+                case "Golden R/S 2":
+                    task.execute(drawable.ic_guide_golden_fusion2_4_3);
+                    break;
+                case "Golden Triangle":
+                    task.execute(drawable.ic_guide_golden_triangle_4_3);
+                    break;
+                case "Group POV Five":
+                    task.execute(drawable.ic_guide_groufie_five);
+                    break;
+                case "Group POV Three":
+                    task.execute(drawable.ic_guide_groufie_three);
+                    break;
+                case "Group POV Potrait":
+                    task.execute(drawable.ic_guide_groupshot_potrait);
+                    break;
+                case "Group POV Full":
+                    task.execute(drawable.ic_guide_groupshot_fullbody);
+                    break;
+                case "Group POV Elvated":
+                    task.execute(drawable.ic_guide_groupshot_elevated_pov);
+                    break;
+                case "Group by Depth":
+                    task.execute(drawable.ic_guide_groupshot_outfocusing);
+                    break;
+                case "Group Center Lead":
+                    task.execute(drawable.ic_guide_groupshot_center_leader);
+                    break;
+                case "Center Type x":
+                    task.execute(drawable.ic_guide_center_type_1_4_3);
+                    break;
+                case "Center Type +":
+                    task.execute(drawable.ic_guide_center_type_2_4_3);
+                    break;
+                case "None":
+                    img.setImageBitmap(null);
+                    break;
+            }
+            img.invalidate();
+        }
+        else
+        {
+            switch (str) {
+                case "Golden Spiral":
+                    task.execute(drawable.ic_guide_golden_spiral_16_9);
+                    break;
+                case "Golden Triangle":
+                    task.execute(drawable.ic_golden_triangle_16_9);
+                    break;
+                case "Rule Of Thirds":
+                    task.execute(drawable.ic_guide_rule_3rd_16_9);
+                    break;
+                case "Center Type x":
+                    task.execute(drawable.ic_guide_center_type_1_4_3);
+                    break;
+                case "Center Type +":
+                    task.execute(drawable.ic_guide_center_type_2_4_3);
+                    break;
+                case "Square 1:1":
+                    task.execute(drawable.ic_guide_insta_1_1);
+                    break;
+                case "Square 4:3":
+                    task.execute(drawable.ic_guide_insta_4_3);
+                    break;
+                case "Square 16:9":
+                    task.execute(drawable.ic_guide_insta_16_9);
+                    break;
+                case "None":
+                    img.setImageBitmap(null);
+                    break;
+            }
+            img.invalidate();
 
-        img.post(new Runnable() {
-                @Override
-                public void run() {
-                    if (quckRationMath < 1.44f) {
-                        switch (str) {
-                            case "Golden Spiral":
-                                img.setImageResource(drawable.ic_guide_golden_spiral_4_3);
-                                break;
-                            case "Rule Of Thirds":
-                                img.setImageResource(drawable.ic_guide_rule_3rd_4_3);
-                                break;
-                            case "Square 1:1":
-                                img.setImageResource(drawable.ic_guide_insta_1_1);
-                                break;
-                            case "Square 4:3":
-                                img.setImageResource(drawable.ic_guide_insta_4_3);
-                                break;
-                            case "Square 16:9":
-                                img.setImageResource(drawable.ic_guide_insta_16_9);
-                                break;
-                            case "Diagonal Type 1":
-                                img.setImageResource(drawable.ic_guide_diagonal_type_1_4_3);
-                                break;
-                            case "Diagonal Type 2":
-                                img.setImageResource(drawable.ic_guide_diagonal_type_2_4_3);
-                                break;
-                            case "Diagonal Type 3":
-                                img.setImageResource(drawable.ic_guide_diagonal_type_3);
-                                break;
-                            case "Diagonal Type 4":
-                                img.setImageResource(drawable.ic_guide_diagonal_type_4);
-                                break;
-                            case "Diagonal Type 5":
-                                img.setImageResource(drawable.ic_guide_diagonal_type_5);
-                                break;
-                            case "Golden Ratio":
-                                img.setImageResource(drawable.ic_guide_golden_ratio_type_1_4_3);
-                                break;
-                            case "Golden Hybrid":
-                                img.setImageResource(drawable.ic_guide_golden_spriral_ratio_4_3);
-                                break;
-                            case "Golden R/S 1":
-                                img.setImageResource(drawable.ic_guide_golden_fuse1_4_3);
-                                break;
-                            case "Golden R/S 2":
-                                img.setImageResource(drawable.ic_guide_golden_fusion2_4_3);
-                                break;
-                            case "Golden Triangle":
-                                img.setImageResource(drawable.ic_guide_golden_triangle_4_3);
-                                break;
-                            case "Group POV Five":
-                                img.setImageResource(drawable.ic_guide_groufie_five);
-                                break;
-                            case "Group POV Three":
-                                img.setImageResource(drawable.ic_guide_groufie_three);
-                                break;
-                            case "Group POV Potrait":
-                                img.setImageResource(drawable.ic_guide_groupshot_potrait);
-                                break;
-                            case "Group POV Full":
-                                img.setImageResource(drawable.ic_guide_groupshot_fullbody);
-                                break;
-                            case "Group POV Elvated":
-                                img.setImageResource(drawable.ic_guide_groupshot_elevated_pov);
-                                break;
-                            case "Group by Depth":
-                                img.setImageResource(drawable.ic_guide_groupshot_outfocusing);
-                                break;
-                            case "Group Center Lead":
-                                img.setImageResource(drawable.ic_guide_groupshot_center_leader);
-                                break;
-                            case "Center Type x":
-                                img.setImageResource(drawable.ic_guide_center_type_1_4_3);
-                                break;
-                            case "Center Type +":
-                                img.setImageResource(drawable.ic_guide_center_type_2_4_3);
-                                break;
-                            case "None":
-                                img.setImageBitmap(null);
-                                break;
-                        }
-                        img.invalidate();
-                    }
-                    else
-                    {
-                        switch (str) {
-                            case "Golden Spiral":
-                                img.setImageResource(drawable.ic_guide_golden_spiral_16_9);
-                                break;
-                            case "Golden Triangle":
-                                img.setImageResource(drawable.ic_golden_triangle_16_9);
-                                break;
-                            case "Rule Of Thirds":
-                                img.setImageResource(drawable.ic_guide_rule_3rd_16_9);
-                                break;
-                            case "Center Type x":
-                                img.setImageResource(drawable.ic_guide_center_type_1_4_3);
-                                break;
-                            case "Center Type +":
-                                img.setImageResource(drawable.ic_guide_center_type_2_4_3);
-                                break;
-                            case "Square 1:1":
-                                img.setImageResource(drawable.ic_guide_insta_1_1);
-                                break;
-                            case "Square 4:3":
-                                img.setImageResource(drawable.ic_guide_insta_4_3);
-                                break;
-                            case "Square 16:9":
-                                img.setImageResource(drawable.ic_guide_insta_16_9);
-                                break;
-                            case "None":
-                                img.setImageBitmap(null);
-                                break;
-                        }
-                        img.invalidate();
-
-                    }
-                }
-            });
+        }
     }
+
 
     @Override
     public void onParameterValueChanged(String val) {
@@ -249,5 +251,6 @@ public class GuideHandler extends Fragment implements I_ModeParameterEvent , I_P
         if (cameraUiWrapper.GetParameterHandler().PreviewSize != null)
             cameraUiWrapper.GetParameterHandler().PreviewSize.addEventListner(previewSizeChanged);
     }
+
 }
 
