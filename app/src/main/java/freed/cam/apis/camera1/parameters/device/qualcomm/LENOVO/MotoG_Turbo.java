8291915324acc95237ed05d566261d2176690eed
defcomk <defcomk@gmail.com>
2016-11-25 00:01:58 +0200

    MotoG_Turbo
    (cherry picked from commit 33e1d0fe6b399ca7f8bd577189b11fa7f0084411)

---
+++ app/src/main/java/freed/cam/apis/camera1/parameters/device/qualcomm/LENOVO/MotoG_Turbo.java
@@ -0,0 +1,42 @@
+package freed.cam.apis.camera1.parameters.device.qualcomm.LENOVO;
+
+import android.hardware.Camera;
+
+import freed.cam.apis.basecamera.CameraWrapperInterface;
+import freed.cam.apis.basecamera.parameters.modes.AbstractModeParameter;
+import freed.cam.apis.basecamera.parameters.modes.MatrixChooserParameter;
+import freed.cam.apis.camera1.parameters.device.BaseQcomNew;
+import freed.cam.apis.camera1.parameters.modes.OpCodeParameter;
+import freed.dng.DngProfile;
+
+/**
+ * Created by GeorgeKiarie on 11/24/2016.
+ */
+public class MotoG_Turbo extends BaseQcomNew {
+
+
+    public MotoG_Turbo(Camera.Parameters parameters, CameraWrapperInterface cameraUiWrapper) {
+        super(parameters, cameraUiWrapper);
+    }
+
+    @Override
+    public boolean IsDngSupported() {
+        return true;
+    }
+
+    @Override
+    public DngProfile getDngProfile(int filesize)
+    {
+        switch (filesize)
+        {
+            case 17326080:
+                return new DngProfile(0, 4164, 3120, DngProfile.Qcom, DngProfile.GRBG, 0, matrixChooserParameter.GetCustomMatrix(MatrixChooserParameter.NEXUS6));
+        }
+        return null;
+    }
+
+    @Override
+    public AbstractModeParameter getOpCodeParameter() {
+        return new OpCodeParameter(cameraUiWrapper.GetAppSettingsManager());
+    }
+}
