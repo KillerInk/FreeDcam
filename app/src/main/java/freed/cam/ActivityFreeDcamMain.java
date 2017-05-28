1b10df99e5150fae254e4d555b892ffbb94339eb
troop <troop@email.de>
2017-05-28 17:42:06 +0200

    change how images gets loaded to make sure bitmaphandler is not null

--- app/src/main/java/freed/cam/ActivityFreeDcamMain.java
+++ app/src/main/java/freed/cam/ActivityFreeDcamMain.java
@@ -174,7 +174,13 @@ public class ActivityFreeDcamMain extends ActivityAbstract
         mPager.setCurrentItem(1);
 
         nightoverlay = (LinearLayout) findViewById(id.nightoverlay);
-        getPermissionHandler().hasExternalSDPermission(onExtSDPermission);
+        if(getPermissionHandler().hasExternalSDPermission(onExtSDPermission))
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    LoadFreeDcamDCIMDirsFiles();
+                }
+            }).start();
         //listen to phone orientation changes
         orientationHandler = new OrientationHandler(this, this);
         orientationHandler.Start();
@@ -252,12 +258,7 @@ public class ActivityFreeDcamMain extends ActivityAbstract
             return;
         loadCameraFragment();
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                LoadFreeDcamDCIMDirsFiles();
-            }
-        }).start();
+
 
         if (getAppSettings().getApiString(AppSettingsManager.SETTING_LOCATION).equals(getAppSettings().getResString(R.string.on_)))
             getPermissionHandler().hasLocationPermission(onLocationPermission);
