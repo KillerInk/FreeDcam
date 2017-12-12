3f0695d7bbfa68a7f5f1d6d82f6a79adb25dbf39
Theliel <git@theliel.es>
2017-12-12 04:27:21 +0100

    Fix GPS Latitude/Longitude for negative coordinates (West or South)

--- app/src/main/jni/freedcam/DngWriter.cpp
+++ app/src/main/jni/freedcam/DngWriter.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <string.h>
+#include <math.h>
 #include "DngWriter.h"
 
 //#define LOG_RAW_DATA
@@ -166,29 +167,37 @@ void DngWriter::makeGPS_IFD(TIFF *tif) {
     {
         LOGD("TIFFCreateGPSDirectory() failed" );
     }
-//    const char longitudeRef = *Longitude  < 0 ? 'W' : 'E';
-//    if (!TIFFSetField( tif, GPSTAG_GPSLongitudeRef, longitudeRef))
-//    {
+    const char* longitudeRef = "E";
+    if (Longitude[0] < 0) {
+        longitudeRef = "W";
+        Longitude[0] = fabsf(Longitude[0]);
+    }
+    if (!TIFFSetField( tif, GPSTAG_GPSLongitudeRef, longitudeRef))
+    {
         LOGD("Can't write LongitudeRef" );
-//    }
-//    LOGD("LONG REF Written %c", longitudeRef);
+    }
+    LOGD("LONG REF Written %c", longitudeRef);
 
-//    if (!TIFFSetField(tif, GPSTAG_GPSLongitude, Longitude))
-//    {
+    if (!TIFFSetField(tif, GPSTAG_GPSLongitude, Longitude))
+    {
         LOGD("Can't write Longitude" );
-//    }
+    }
     LOGD("Longitude Written");
-//    const char latitudeRef = *Latitude < 0 ? 'S' : 'N';
-//    LOGD("PMETH Written");
-//    if (!TIFFSetField( tif, GPSTAG_GPSLatitudeRef, latitudeRef)) {
+    const char* latitudeRef = "N";
+    if (Latitude[0] < 0) {
+        latitudeRef = "S";
+        Latitude[0] = fabsf(Latitude[0]);
+    }
+    LOGD("PMETH Written");
+    if (!TIFFSetField( tif, GPSTAG_GPSLatitudeRef, latitudeRef)) {
         LOGD("Can't write LAti REf" );
-//    }
-//    LOGD("LATI REF Written %c", latitudeRef);
+    }
+    LOGD("LATI REF Written %c", latitudeRef);
 
- //   if (!TIFFSetField( tif, GPSTAG_GPSLatitude,Latitude))
-//    {
+    if (!TIFFSetField( tif, GPSTAG_GPSLatitude,Latitude))
+    {
         LOGD("Can't write Latitude" );
-//    }
+    }
     LOGD("Latitude Written");
     if (!TIFFSetField( tif, GPSTAG_GPSAltitude,Altitude))
     {
