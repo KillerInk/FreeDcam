8df9bbc8861995df28daccfdff034e5748ac65f7
KillerInk <KillerInk@email.de>
2019-10-30 05:54:11 +0100

    fixup CellStormModule

--- app/src/main/java/freed/cam/apis/camera2/modules/CellStormModule.java
+++ app/src/main/java/freed/cam/apis/camera2/modules/CellStormModule.java
@@ -4,7 +4,11 @@ import android.os.Handler;
 
 import com.troop.freedcam.R;
 
+import java.io.File;
+
 import freed.cam.apis.basecamera.CameraWrapperInterface;
+import freed.settings.SettingsManager;
+import freed.utils.Log;
 
 public class CellStormModule extends PictureModuleApi2 {
 
@@ -12,9 +16,11 @@ public class CellStormModule extends PictureModuleApi2 {
     private boolean continueCapture = false;
     private final int cropSize = 100;
 
+    private final  String TAG = CellStormModule.class.getSimpleName();
+
     public CellStormModule(CameraWrapperInterface cameraUiWrapper, Handler mBackgroundHandler, Handler mainHandler) {
         super(cameraUiWrapper, mBackgroundHandler, mainHandler);
-        name = cameraUiWrapper.getResString(R.string.module_cellstorm);
+        name = SettingsManager.getInstance().getResString(R.string.module_cellstorm);
     }
 
     @Override
@@ -43,10 +49,19 @@ public class CellStormModule extends PictureModuleApi2 {
     }
 
     @Override
-    protected void finishCapture() {
-        if (!continueCapture)
-            super.finishCapture();
-        else
-            captureStillPicture();
+    public void internalFireOnWorkDone(File file)
+    {
+            Log.d(TAG, "internalFireOnWorkDone BurstCount:" + BurstCounter.getBurstCount() + " imageCount:" + BurstCounter.getImageCaptured());
+            if (continueCapture) {
+                filesSaved.add(file);
+                captureStillPicture();
+                Log.d(TAG, "internalFireOnWorkDone Burst addFile");
+            }
+            else {
+                Log.d(TAG, "internalFireOnWorkDone Burst done");
+                fireOnWorkFinish(filesSaved.toArray(new File[filesSaved.size()]));
+                filesSaved.clear();
+            }
     }
+
 }
