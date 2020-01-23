a7785d99218b7a36216dfe36c8d215d02e538516
KillerInk <KillerInk@email.de>
2020-01-23 13:34:33 +0100

    camera2 dump ImageFormat output sizes

--- app/src/main/java/freed/cam/apis/featuredetector/Camera2FeatureDetectorTask.java
+++ app/src/main/java/freed/cam/apis/featuredetector/Camera2FeatureDetectorTask.java
@@ -95,6 +95,8 @@ public class Camera2FeatureDetectorTask extends AbstractFeatureDetectorTask {
 
                 publishProgress("Camera 2 Level:" + hwlvl);
 
+                dump_SCALER_STREAM_CONFIGURATION_MAP(characteristics);
+
                 //check first if a already checked cam have camera2features and if its now the front cam that dont have a camera2feature.
                 //in that case set it to true
                 //else it would override the already detected featureset from last cam and disable api2
@@ -1172,6 +1174,91 @@ public class Camera2FeatureDetectorTask extends AbstractFeatureDetectorTask {
             ar.add(i + "");
         }
         return ar;
+    }
+
+    private void dump_SCALER_STREAM_CONFIGURATION_MAP(CameraCharacteristics characteristics)
+    {
+        StreamConfigurationMap smap =  characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+        int outputformats[] =  smap.getOutputFormats();
+        String out;
+        for(int outformat : outputformats)
+        {
+            switch (outformat)
+            {
+                case ImageFormat.DEPTH16:
+                    Log.d(TAG,"ImageFormat.DEPTH16 : " + logResForFormat(smap,ImageFormat.DEPTH16));
+                    break;
+                case ImageFormat.DEPTH_JPEG:
+                    Log.d(TAG,"ImageFormat.DEPTH_JPEG : " + logResForFormat(smap,ImageFormat.DEPTH_JPEG));
+                    break;
+                case ImageFormat.DEPTH_POINT_CLOUD:
+                    Log.d(TAG,"ImageFormat.DEPTH_POINT_CLOUD : " + logResForFormat(smap,ImageFormat.DEPTH_POINT_CLOUD));
+                    break;
+                case ImageFormat.FLEX_RGB_888:
+                    Log.d(TAG,"ImageFormat.FLEX_RGB_888 : " + logResForFormat(smap,ImageFormat.FLEX_RGB_888));
+                    break;
+                case ImageFormat.FLEX_RGBA_8888:
+                    Log.d(TAG,"ImageFormat.FLEX_RGBA_8888 : " + logResForFormat(smap,ImageFormat.FLEX_RGBA_8888));
+                    break;
+                case ImageFormat.HEIC:
+                    Log.d(TAG,"ImageFormat.HEIC : " + logResForFormat(smap,ImageFormat.HEIC));
+                    break;
+                case ImageFormat.JPEG:
+                    Log.d(TAG,"ImageFormat.JPEG : " + logResForFormat(smap,ImageFormat.JPEG));
+                    break;
+                case ImageFormat.NV16:
+                    Log.d(TAG,"ImageFormat.NV16 : " + logResForFormat(smap,ImageFormat.NV16));
+                    break;
+                case ImageFormat.NV21:
+                    Log.d(TAG,"ImageFormat.NV21 : " + logResForFormat(smap,ImageFormat.NV21));
+                    break;
+                case ImageFormat.PRIVATE:
+                    Log.d(TAG,"ImageFormat.NV21 : " + logResForFormat(smap,ImageFormat.PRIVATE));
+                    break;
+                case ImageFormat.RAW10:
+                    Log.d(TAG,"ImageFormat.RAW10 : " + logResForFormat(smap,ImageFormat.RAW10));
+                    break;
+                case ImageFormat.RAW12:
+                    Log.d(TAG,"ImageFormat.RAW12 : " + logResForFormat(smap,ImageFormat.RAW12));
+                    break;
+                case ImageFormat.RAW_PRIVATE:
+                    Log.d(TAG,"ImageFormat.RAW_PRIVATE : " + logResForFormat(smap,ImageFormat.RAW_PRIVATE));
+                    break;
+                case ImageFormat.RAW_SENSOR:
+                    Log.d(TAG,"ImageFormat.RAW_SENSOR : " + logResForFormat(smap,ImageFormat.RAW_SENSOR));
+                    break;
+                case ImageFormat.RGB_565:
+                    Log.d(TAG,"ImageFormat.RGB_565 : " + logResForFormat(smap,ImageFormat.RGB_565));
+                    break;
+                case ImageFormat.UNKNOWN:
+                    Log.d(TAG,"ImageFormat.UNKNOWN : " + logResForFormat(smap,ImageFormat.UNKNOWN));
+                    break;
+                case ImageFormat.Y8:
+                    Log.d(TAG,"ImageFormat.Y8 : " + logResForFormat(smap,ImageFormat.Y8));
+                    break;
+                case ImageFormat.YUV_420_888:
+                    Log.d(TAG,"ImageFormat.YUV_420_888 : " + logResForFormat(smap,ImageFormat.YUV_420_888));
+                    break;
+                case ImageFormat.YUV_422_888:
+                    Log.d(TAG,"ImageFormat.YUV_422_888 : " + logResForFormat(smap,ImageFormat.YUV_422_888));
+                    break;
+                case ImageFormat.YUV_444_888:
+                    Log.d(TAG,"ImageFormat.YUV_444_888 : " + logResForFormat(smap,ImageFormat.YUV_444_888));
+                    break;
+                case ImageFormat.YUY2:
+                    Log.d(TAG,"ImageFormat.YUY2 : " + logResForFormat(smap,ImageFormat.YUY2));
+                    break;
+                case ImageFormat.YV12:
+                    Log.d(TAG,"ImageFormat.YV12 : " + logResForFormat(smap,ImageFormat.YV12));
+                    break;
+            }
+        }
+    }
+
+    private String logResForFormat(StreamConfigurationMap smap, int imageFormat)
+    {
+        Size[] sizes =  smap.getOutputSizes(imageFormat);
+        return Arrays.toString(sizes);
     }
 
     private void detectVideoMediaProfiles(int cameraid)
