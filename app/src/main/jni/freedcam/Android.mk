7edf5877ae89f3c0d93d1d12494720036cac4f80
KillerInk <KillerInk@email.de>
2020-05-04 13:14:27 +0200

    remove openmp, its not needed

--- app/src/main/jni/freedcam/Android.mk
+++ app/src/main/jni/freedcam/Android.mk
@@ -6,13 +6,11 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := libfreedcam
 LOCAL_SRC_FILES := RawToDng.cpp librawutils.cpp DngWriter.cpp DngStacker.cpp ExifInfo.cpp GpsInfo.cpp DngProfile.cpp CustomMatrix.cpp OpCode.cpp
 LOCAL_CPPFLAGS := -fexceptions -fopenmp -Wno-c++11-narrowing -frtti -std=c++11 -fpic
-LOCAL_CFLAGS += -fopenmp
-LOCAL_LDFLAGS += -fopenmp
+
 LOCAL_LDLIBS := -lz \
 	-L $(LOCAL_PATH)/libs \
 	-lm \
 	-llog \
-	-fopenmp \
 	-ljnigraphics \
 	-landroid
 LOCAL_C_INCLUDES += \
