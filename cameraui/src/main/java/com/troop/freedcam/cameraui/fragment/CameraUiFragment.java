f7f689fa73f0cac17927bf3f10e25744ff25f1be
KillerInk <KillerInk@email.de>
2020-10-31 15:57:59 +0100

    bind left and right bar

--- cameraui/src/main/java/com/troop/freedcam/cameraui/fragment/CameraUiFragment.java
+++ cameraui/src/main/java/com/troop/freedcam/cameraui/fragment/CameraUiFragment.java
@@ -47,7 +47,27 @@ public class CameraUiFragment extends Fragment {
         cameraUiFragmentBinding.cameraUiManuals.manualTonecurve.manualButtonBinding.setManualButtonModel(mViewModel.getManualButtonModel(CameraUiViewModel.ManualButtons.tonecurve));
         cameraUiFragmentBinding.cameraUiManuals.manualWhitebalance.manualButtonBinding.setManualButtonModel(mViewModel.getManualButtonModel(CameraUiViewModel.ManualButtons.wb));
         cameraUiFragmentBinding.cameraUiManuals.manualZoom.manualButtonBinding.setManualButtonModel(mViewModel.getManualButtonModel(CameraUiViewModel.ManualButtons.zoom));
-        //
+        //bind leftbar
+        cameraUiFragmentBinding.cameraUiLeftBar.histogramButton.manualButtonBinding.setManualButtonModel(mViewModel.getManualButtonModel(CameraUiViewModel.LeftbarButtons.histogram));
+        cameraUiFragmentBinding.cameraUiLeftBar.clippingButton.manualButtonBinding.setManualButtonModel(mViewModel.getManualButtonModel(CameraUiViewModel.LeftbarButtons.clipping));
+        cameraUiFragmentBinding.cameraUiLeftBar.whitebalanceModeButton.manualButtonBinding.setManualButtonModel(mViewModel.getManualButtonModel(CameraUiViewModel.LeftbarButtons.wb));
+        cameraUiFragmentBinding.cameraUiLeftBar.isoModeButton.manualButtonBinding.setManualButtonModel(mViewModel.getManualButtonModel(CameraUiViewModel.LeftbarButtons.iso));
+        cameraUiFragmentBinding.cameraUiLeftBar.flashModeButton.manualButtonBinding.setManualButtonModel(mViewModel.getManualButtonModel(CameraUiViewModel.LeftbarButtons.flash));
+        cameraUiFragmentBinding.cameraUiLeftBar.focusModeButton.manualButtonBinding.setManualButtonModel(mViewModel.getManualButtonModel(CameraUiViewModel.LeftbarButtons.focus));
+        cameraUiFragmentBinding.cameraUiLeftBar.exposureModeButton.manualButtonBinding.setManualButtonModel(mViewModel.getManualButtonModel(CameraUiViewModel.LeftbarButtons.exposure));
+        cameraUiFragmentBinding.cameraUiLeftBar.contshotModeButton.manualButtonBinding.setManualButtonModel(mViewModel.getManualButtonModel(CameraUiViewModel.LeftbarButtons.contshot));
+        cameraUiFragmentBinding.cameraUiLeftBar.hdrModeButton.manualButtonBinding.setManualButtonModel(mViewModel.getManualButtonModel(CameraUiViewModel.LeftbarButtons.hdr));
+        cameraUiFragmentBinding.cameraUiLeftBar.nightModeButton.manualButtonBinding.setManualButtonModel(mViewModel.getManualButtonModel(CameraUiViewModel.LeftbarButtons.night));
+        cameraUiFragmentBinding.cameraUiLeftBar.pictureModeButton.manualButtonBinding.setManualButtonModel(mViewModel.getManualButtonModel(CameraUiViewModel.LeftbarButtons.imageformat));
+
+        //bind rightbar
+        cameraUiFragmentBinding.cameraUiRightBar.closeButton.manualButtonBinding.setManualButtonModel(mViewModel.getManualButtonModel(CameraUiViewModel.RightbarButtons.close));
+        cameraUiFragmentBinding.cameraUiRightBar.moduleswitchButton.manualButtonBinding.setManualButtonModel(mViewModel.getManualButtonModel(CameraUiViewModel.RightbarButtons.module));
+        cameraUiFragmentBinding.cameraUiRightBar.focuspeakButton.manualButtonBinding.setManualButtonModel(mViewModel.getManualButtonModel(CameraUiViewModel.RightbarButtons.focuspeak));
+        cameraUiFragmentBinding.cameraUiRightBar.selftimerButton.manualButtonBinding.setManualButtonModel(mViewModel.getManualButtonModel(CameraUiViewModel.RightbarButtons.selftimer));
+        cameraUiFragmentBinding.cameraUiRightBar.aelockButton.manualButtonBinding.setManualButtonModel(mViewModel.getManualButtonModel(CameraUiViewModel.RightbarButtons.aelock));
+        cameraUiFragmentBinding.cameraUiRightBar.cameraSwitchButton.manualButtonBinding.setManualButtonModel(mViewModel.getManualButtonModel(CameraUiViewModel.RightbarButtons.cameraswitch));
+
         return cameraUiFragmentBinding.getRoot();
     }
 
--- cameraui/src/main/java/com/troop/freedcam/cameraui/viewmodels/CameraUiViewModel.java
+++ cameraui/src/main/java/com/troop/freedcam/cameraui/viewmodels/CameraUiViewModel.java
@@ -27,17 +27,65 @@ public class CameraUiViewModel extends ViewModel {
         burst,
     }
 
+    public enum LeftbarButtons
+    {
+        histogram,
+        clipping,
+        wb,
+        iso,
+        flash,
+        focus,
+        exposure,
+        aepriority,
+        contshot,
+        hdr,
+        night,
+        imageformat,
+    }
+
+    public enum RightbarButtons
+    {
+        close,
+        module,
+        focuspeak,
+        selftimer,
+        aelock,
+        cameraswitch,
+    }
+
     private HashMap<ManualButtons, ManualButtonModel> manualButtonModelHashMap;
+    private HashMap<LeftbarButtons, ManualButtonModel> leftbarButtonsManualButtonModelHashMap;
+    private HashMap<RightbarButtons, ManualButtonModel> rightbarButtonsManualButtonModelHashMap;
 
     public CameraUiViewModel() {
         manualButtonModelHashMap = new HashMap<>();
         ManualButtons buttons[] = ManualButtons.values();
         for (ManualButtons b : buttons)
             manualButtonModelHashMap.put(b, new ManualButtonModel());
+        leftbarButtonsManualButtonModelHashMap = new HashMap<>();
+        LeftbarButtons leftbarButtons[] = LeftbarButtons.values();
+        for (LeftbarButtons buttons1: leftbarButtons)
+            leftbarButtonsManualButtonModelHashMap.put(buttons1, new ManualButtonModel());
+
+        rightbarButtonsManualButtonModelHashMap = new HashMap<>();
+        RightbarButtons rightbarButtons[] = RightbarButtons.values();
+        for (RightbarButtons buttons1: rightbarButtons)
+            rightbarButtonsManualButtonModelHashMap.put(buttons1, new ManualButtonModel());
+
     }
 
     public ManualButtonModel getManualButtonModel(ManualButtons manualButtons)
     {
         return manualButtonModelHashMap.get(manualButtons);
+    }
+
+    public ManualButtonModel getManualButtonModel(LeftbarButtons manualButtons)
+    {
+        return leftbarButtonsManualButtonModelHashMap.get(manualButtons);
+    }
+
+    public ManualButtonModel getManualButtonModel(RightbarButtons manualButtons)
+    {
+        return rightbarButtonsManualButtonModelHashMap.get(manualButtons);
     }
 }
