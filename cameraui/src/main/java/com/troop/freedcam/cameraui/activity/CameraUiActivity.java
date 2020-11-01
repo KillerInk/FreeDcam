0ee196d7d9c5d56c3cd10fa86751df225395aeb8
KillerInk <KillerInk@email.de>
2020-11-01 11:41:42 +0100

    full screen activity

--- cameraui/src/main/java/com/troop/freedcam/cameraui/activity/CameraUiActivity.java
+++ cameraui/src/main/java/com/troop/freedcam/cameraui/activity/CameraUiActivity.java
@@ -6,17 +6,30 @@ import android.os.Bundle;
 
 import com.troop.freedcam.cameraui.R;
 import com.troop.freedcam.cameraui.fragment.MainFragment;
+import com.troop.freedcam.cameraui.utils.HideNavBarHelper;
 
 public class CameraUiActivity extends AppCompatActivity {
+
+    private HideNavBarHelper hideNavBarHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.camera_ui_activity);
+        hideNavBarHelper = new HideNavBarHelper();
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.container, MainFragment.newInstance())
                     .commitNow();
         }
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus)
+            hideNavBarHelper.HIDENAVBAR(getWindow());
+        else
+            hideNavBarHelper.showNavbar(getWindow());
     }
 }
---
+++ cameraui/src/main/java/com/troop/freedcam/cameraui/utils/HideNavBarHelper.java
@@ -0,0 +1,60 @@
+package com.troop.freedcam.cameraui.utils;
+
+import android.os.Build;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class HideNavBarHelper
+{
+    private final int hideflags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+
+    private final int showflags = View.SYSTEM_UI_FLAG_VISIBLE;
+
+
+
+    public void HIDENAVBAR(Window window)
+    {
+        if (Build.VERSION.SDK_INT < 16) {
+            window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        }
+        else
+        {
+            //HIDE nav and action bar
+            View decorView = window.getDecorView();
+            decorView.setSystemUiVisibility(hideflags);
+            decorView.setOnSystemUiVisibilityChangeListener(visibility -> {
+                if (visibility > 0) {
+                    if (Build.VERSION.SDK_INT >= 16)
+                        window.getDecorView().setSystemUiVisibility(hideflags);
+                }
+            });
+        }
+    }
+
+    public void showNavbar(Window window)
+    {
+        if (Build.VERSION.SDK_INT < 16) {
+            window.clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+        }
+        else
+        {
+            //HIDE nav and action bar
+            View decorView = window.getDecorView();
+            decorView.setSystemUiVisibility(showflags);
+            decorView.setOnSystemUiVisibilityChangeListener(visibility -> {
+                if (visibility > 0) {
+                    if (Build.VERSION.SDK_INT >= 16)
+                        window.getDecorView().setSystemUiVisibility(showflags);
+                }
+            });
+        }
+    }
+}
