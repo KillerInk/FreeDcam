0eb420f5e1f5f623dbdc9282d3da5f5783806116
killerink <killerink@email.de>
2020-12-25 12:08:16 +0100

    extracted mi opcode,PixelFormat

---
+++ app/src/main/java/camera2_hidden_keys/xiaomi/PixelFormat.java
@@ -0,0 +1,8 @@
+package camera2_hidden_keys.xiaomi;
+
+public class PixelFormat {
+    public static final int HAL_PIXEL_FORMAT_BLOB = 33;
+    public static final int HAL_PIXEL_FORMAT_IMPLEMENTATION_DEFINED = 34;
+    public static final int HAL_PIXEL_FORMAT_YCbCr_420_888 = 35;
+
+}
