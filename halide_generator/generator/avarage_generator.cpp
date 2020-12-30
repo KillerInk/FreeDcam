b14f9d0eefca194d114199d7b0c18a4cd1e18b82
KillerInk <KillerInk@email.de>
2020-12-30 21:37:07 +0100

    more not working gpu trys

--- halide_generator/generator/avarage_generator.cpp
+++ halide_generator/generator/avarage_generator.cpp
@@ -6,16 +6,160 @@ using namespace Halide;
 namespace {
 
     class Avarage_generator : public Generator<Avarage_generator> {
+
     public:
         Input <Halide::Buffer<uint16_t>> inputs{"inputs", 3};
         // Merged buffer
         Output <Halide::Buffer<uint16_t>> output{"output", 2};
 
-        Var x,y,n;
+        Var x{"x"},y{"y"};
+        Var block2{"block2"}, thread2{"thread2"},xo{"xo"}, yo{"yo"}, xi{"xi"}, yi{"yi"};
 
         void generate() {
 
-            output(x,y) = (inputs(x,y,0) + inputs(x,y,1)/2);
+            Func merged{"merged"};
+            merged(x,y) =  (inputs(x,y,0) + inputs(x,y,1))/2;
+            output(x,y) = merged(x,y);
+
+
+
+            /*output.split(x,block2, thread2 ,16);
+            output.gpu_blocks(block2).gpu_threads(thread2);
+            merged.compute_root();*/
+            /**
+             * produce merged:
+                  for y:
+                    for x:
+                      merged(...) = ...
+                consume merged:
+                  produce output:
+                    for y:
+                      gpu_block x.block2<Default_GPU>:
+                        gpu_thread x.thread2 in [0, 15]<Default_GPU>:
+                          output(...) = ...
+             */
+
+            /*output.split(x,block2, thread2 ,16);
+            output.gpu_blocks(block2).gpu_threads(thread2);*/
+            /**
+             * produce output:
+                  for y:
+                    gpu_block x.block2<Default_GPU>:
+                      gpu_thread x.thread2 in [0, 15]<Default_GPU>:
+                        output(...) = ...
+             */
+
+            /*
+            output.split(x,block2, thread2 ,16);
+            output.parallel(y).gpu_blocks(block2).gpu_threads(thread2);*/
+            /**
+             * produce output:
+                  parallel y:
+                    gpu_block x.block2<Default_GPU>:
+                      gpu_thread x.thread2 in [0, 15]<Default_GPU>:
+                        output(...) = ...
+             */
+
+
+            /*output.gpu_tile(x, y, xo, yo, xi, yi, 16, 16);
+            merged.compute_at(output, xo);
+            merged.gpu_threads(x, y);*/
+            /**
+             * produce output:
+              gpu_block y.yo<Default_GPU>:
+                gpu_block x.xo<Default_GPU>:
+                  produce merged:
+                    gpu_thread y<Default_GPU>:
+                      gpu_thread x<Default_GPU>:
+                        merged(...) = ...
+                  consume merged:
+                    gpu_thread y.yi in [0, 15]<Default_GPU>:
+                      gpu_thread x.xi in [0, 15]<Default_GPU>:
+                        output(...) = ...
+             */
+
+           /* output.gpu_tile(x, y, xo, yo, xi, yi, 16, 16);
+            output.compute_root();*/
+            /**
+             * produce output:
+                  gpu_block y.yo<Default_GPU>:
+                    gpu_block x.xo<Default_GPU>:
+                      gpu_thread y.yi in [0, 15]<Default_GPU>:
+                        gpu_thread x.xi in [0, 15]<Default_GPU>:
+                          output(...) = ...
+             */
+
+
+            /*merged.gpu_tile(x, y, xo, yo, xi, yi, 16, 16);
+            merged.compute_root();*/
+            /**
+             * produce merged:
+                  gpu_block y.yo<Default_GPU>:
+                    gpu_block x.xo<Default_GPU>:
+                      gpu_thread y.yi in [0, 15]<Default_GPU>:
+                        gpu_thread x.xi in [0, 15]<Default_GPU>:
+                          merged(...) = ...
+                consume merged:
+                  produce output:
+                    for y:
+                      for x:
+                        output(...) = ...
+             */
+
+
+            /*merged.gpu_tile(x, y, xo, yo, xi, yi, 16, 16);
+            output.gpu_tile(x, y, xo, yo, xi, yi, 16, 16);
+            merged.compute_root();*/
+            /**
+             * produce merged:
+                  gpu_block y.yo<Default_GPU>:
+                    gpu_block x.xo<Default_GPU>:
+                      gpu_thread y.yi in [0, 15]<Default_GPU>:
+                        gpu_thread x.xi in [0, 15]<Default_GPU>:
+                          merged(...) = ...
+                consume merged:
+                  produce output:
+                    gpu_block y.yo<Default_GPU>:
+                      gpu_block x.xo<Default_GPU>:
+                        gpu_thread y.yi in [0, 15]<Default_GPU>:
+                          gpu_thread x.xi in [0, 15]<Default_GPU>:
+                            output(...) = ...
+             */
+
+
+
+           /* merged.gpu_tile(x, y, xo, yo, xi, yi, 16, 16);
+            merged.compute_root();
+            output.split(x,block2, thread2 ,16);
+            output.parallel(y).gpu_blocks(block2).gpu_threads(thread2);
+*/
+            /**
+             * produce merged:
+                  gpu_block y.yo<Default_GPU>:
+                    gpu_block x.xo<Default_GPU>:
+                      gpu_thread y.yi in [0, 15]<Default_GPU>:
+                        gpu_thread x.xi in [0, 15]<Default_GPU>:
+                          merged(...) = ...
+                consume merged:
+                  produce output:
+                    parallel y:
+                      gpu_block x.block2<Default_GPU>:
+                        gpu_thread x.thread2 in [0, 15]<Default_GPU>:
+                          output(...) = ...
+             */
+
+            //the only working method that create not a black output
+            output.parallel(y).vectorize(x,128);
+            /**
+             * produce output:
+                  parallel y:
+                    for x.x:
+                      vectorized x.v11 in [0, 15]:
+                        output(...) = ...
+             */
+
+            output.print_loop_nest();
+
         }
     };
 }
