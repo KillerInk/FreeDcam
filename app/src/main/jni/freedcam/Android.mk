5814433158c9ea993d20a89e6fb2600b30186f5a
killerink <killerink@email.de>
2021-01-09 09:49:51 +0100

    try to prefab libtiff

--- app/src/main/jni/freedcam/Android.mk
+++ app/src/main/jni/freedcam/Android.mk
@@ -6,6 +6,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := libfreedcam
 LOCAL_SRC_FILES := RawToDng.cpp DngWriter.cpp DngStacker.cpp ExifInfo.cpp GpsInfo.cpp DngProfile.cpp CustomMatrix.cpp OpCode.cpp LibRawWrapper.cpp LibRawJniWrapper.cpp
 LOCAL_CPPFLAGS := -fexceptions -Wno-c++11-narrowing -frtti -std=c++11 -fpic
+LOCAL_SHARED_LIBRARIES := libtiff
 
 LOCAL_LDLIBS := -lz \
 	-L $(LOCAL_PATH)/libs \
@@ -13,10 +14,14 @@ LOCAL_LDLIBS := -lz \
 	-llog \
 	-ljnigraphics \
 	-landroid
-LOCAL_C_INCLUDES += \
-					$(NDK_APP_PROJECT_PATH)/tiff/libtiff/ \
 
-LOCAL_STATIC_LIBRARIES := libtiff libraw
+
+include $(BUILD_SHARED_LIBRARY)
+
+# Add the prefab modules to the import path.
+$(call import-add-path,/out)
+$(call import-module,prefab/libtiff)
+LOCAL_STATIC_LIBRARIES :=  libraw
 include $(BUILD_SHARED_LIBRARY)
 
 
