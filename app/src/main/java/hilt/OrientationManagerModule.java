8a81bbd1a7263bdf6c911dec41460d7ddefb5dc8
KillerInk <KillerInk@email.de>
2021-09-15 15:59:02 +0200

    fix multiply OrientationManager got created #175

--- app/src/main/java/hilt/OrientationManagerModule.java
+++ app/src/main/java/hilt/OrientationManagerModule.java
@@ -7,6 +7,7 @@ import dagger.Provides;
 import dagger.hilt.InstallIn;
 import dagger.hilt.android.components.ActivityComponent;
 import dagger.hilt.android.qualifiers.ActivityContext;
+import dagger.hilt.android.scopes.ActivityScoped;
 import freed.utils.OrientationManager;
 
 @Module
@@ -14,6 +15,7 @@ import freed.utils.OrientationManager;
 public class OrientationManagerModule {
 
     @Provides
+    @ActivityScoped
     public OrientationManager orientationManager(@ActivityContext Context context)
     {
         return new OrientationManager(context);
