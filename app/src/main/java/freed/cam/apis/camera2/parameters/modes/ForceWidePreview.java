051ec06b22bd3cabf70ea2e6678e6227e27a3ec3
killerink <killerink@email.de>
2022-07-20 19:39:44 +0200

    add ForceWidePreview

---
+++ app/src/main/java/freed/cam/apis/camera2/parameters/modes/ForceWidePreview.java
@@ -0,0 +1,34 @@
+package freed.cam.apis.camera2.parameters.modes;
+
+import freed.cam.apis.basecamera.CameraWrapperInterface;
+import freed.cam.apis.basecamera.parameters.AbstractParameter;
+import freed.cam.apis.camera2.Camera2;
+import freed.settings.SettingKeys;
+import freed.settings.mode.BooleanSettingModeInterface;
+
+public class ForceWidePreview extends AbstractParameter implements BooleanSettingModeInterface {
+
+    CameraWrapperInterface cameraWrapperInterface;
+    BooleanSettingModeInterface booleanSettingModeInterface;
+
+    public ForceWidePreview(CameraWrapperInterface cameraUiWrapper)
+    {
+        super(cameraUiWrapper, SettingKeys.FORCE_WIDE_PREVIEW);
+        this.cameraWrapperInterface = cameraUiWrapper;
+        this.booleanSettingModeInterface = settingsManager.get(SettingKeys.FORCE_WIDE_PREVIEW);
+    }
+
+    @Override
+    public boolean get() {
+        if (booleanSettingModeInterface != null)
+            return booleanSettingModeInterface.get();
+        return false;
+    }
+
+    @Override
+    public void set(boolean bool) {
+        booleanSettingModeInterface.set(bool);
+        cameraWrapperInterface.stopPreview();
+        cameraWrapperInterface.startPreview();
+    }
+}
